5291f645689eb00f46d4088051492f4034624c8b
xiaoleiwei <weixiaolei@zplay.com>
2017-02-24 16:59:43 +0800

    add vungle and adcolony

---
+++ YumiMediationAdapters/Adcolony/YuMIVideoAdAdcolonyNetworkAdapter.h
@@ -0,0 +1,14 @@
+//
+//  YuMIVideoAdAdcolonyNetworkAdapter.h
+//  YUMIVideoSample
+//
+//  Created by wxl on 15/9/21.
+//  Copyright (c) 2015年 AdsYuMI. All rights reserved.
+//
+
+#import "YuMIVideoAdNetworkAdapter.h"
+#import <AdColony/AdColony.h>
+
+@interface YuMIVideoAdAdcolonyNetworkAdapter : YuMIVideoAdNetworkAdapter <AdColonyDelegate,AdColonyAdDelegate>
+
+@end
---
+++ YumiMediationAdapters/Vungle/YuMIVideoAdVungleNetworkAdapter.h
@@ -0,0 +1,13 @@
+//
+//  YuMIVideoAdVungleNetworkAdapter.h
+//  YUMIVideoSample
+//
+//  Created by wxl on 15/9/21.
+//  Copyright (c) 2015年 AdsYuMI. All rights reserved.
+//
+
+#import "YuMIVideoAdNetworkAdapter.h"
+#import <VungleSDK/VungleSDK.h>
+
+@interface YuMIVideoAdVungleNetworkAdapter : YuMIVideoAdNetworkAdapter <VungleSDKDelegate> 
+@end
