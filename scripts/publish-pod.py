03726f66fad32c4d3eaa38a12c9b14996e40f90f
on99 <nanohugh@gmail.com>
2017-03-07 14:09:24 +0800

    Add script skeleton

---
+++ scripts/publish-pod.py
@@ -0,0 +1,71 @@
+import os
+import sys
+
+
+TAG = os.environ.get('TRAVIS_TAG')
+
+def main(argv):
+    yumi_mediation_sdk_version = '~> 0.8.9'
+    adapters = [
+        # Adapter('AdColony', '"AdColony", "2.6.3"', '2.6.3.0'),
+        Adapter('AdMob', '"Google-Mobile-Ads-SDK", "7.18.0"', '7.8.1.0'),
+        Adapter('AppLovin', '"YumiAppLovinSDK", "3.4.3"', '3.4.3.0'),
+        Adapter('Baidu', '"YumiBaiduSDK", "4.5.0"', '4.5.0.0'),
+        Adapter('Chartboost', '"ChartboostSDK", "6.6.1"', '6.6.1.0'),
+        Adapter('Facebook', '"FBAudienceNetwork", "4.17.0"', '4.17.0.0'),
+        Adapter('GDT', '"YumiGDTSDK", "4.5.5"', '4.5.5.0'),
+        Adapter('InMobi', '"InMobiSDK", "6.0.0"', '6.0.0.0'),
+        Adapter('Mopub', '"YumiMopubSDK", "4.11.1"', '4.11.1.0'),
+        Adapter('StartApp', '"YumiStartAppSDK", "3.4.1"', '3.4.1.0'),
+        Adapter('Unity', '"YumiUnitySDK", "2.0.0"', '2.0.0.0'),
+        # Adapter('Vungle', '"VungleSDK-iOS", "4.0.8"', '4.0.8.0')
+    ]
+    for adapter in adapters:
+        podspec_name = 'YumiMediation%s' % adapter.name
+        generate_podspec_for_packaging(podspec_name, adapter.name, yumi_mediation_sdk_version)
+        package(podspec_name, adapter.name)
+        compressed_filename = compress(podspec_name, adapter.version)
+        remote_filename = 'iOS/YumiMediationAdapters/%s' % compressed_filename
+        upload_to_oss(compressed_filename, remote_filename)
+        source = "{ :http => 'http://adsdk.yumimobi.com/%s' }" % remote_filename
+        generate_podspec_for_publishing(podspec_name, adapter, source, yumi_mediation_sdk_version)
+        publish_pod(podspec_name)
+
+
+class Adapter:
+    def __init__(self, name, third_party_sdk_dependency, version):
+        self.name = name
+        self.third_party_sdk_dependency = third_party_sdk_dependency
+        self.version = version
+
+
+def generate_podspec_for_packaging(podspec_name, name, yumi_mediation_sdk_version):
+    pass
+
+
+def package(podspec_name, name):
+    pass
+
+
+def compress(podspec_name, version):
+    pass
+
+
+def upload_to_oss(local_filename, remote_filename):
+    pass
+
+
+def generate_podspec_for_publishing(podspec_name, adapter, source, yumi_mediation_sdk_version):
+    pass
+
+
+def publish_pod(podspec_name):
+    pass
+
+
+if __name__ == "__main__":
+    if not TAG:
+        print('this is not a tag, exit here...')
+        sys.exit(0)
+
+    main(sys.argv)
