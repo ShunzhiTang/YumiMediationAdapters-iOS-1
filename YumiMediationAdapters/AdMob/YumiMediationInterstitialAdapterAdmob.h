5161c204df57233ae80c1b80abc6b4342acb9589
xiaoleiwei <weixiaolei@zplay.com>
2017-06-26 17:50:26 +0800

    amend class name and add isReady method

---
+++ YumiMediationAdapters/AdMob/YumiMediationInterstitialAdapterAdmob.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationInterstitialAdapterAdMob.h
+//  Pods
+//
+//  Created by 魏晓磊 on 17/6/21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationInterstitialAdapterAdMob : NSObject
+
+@end
---
+++ YumiMediationAdapters/AdMob/YumiMediationInterstitialAdapterAdmob.m
@@ -0,0 +1,81 @@
+//
+//  YumiMediationInterstitialAdapterAdMob.m
+//  Pods
+//
+//  Created by 魏晓磊 on 17/6/21.
+//
+//
+
+#import "YumiMediationInterstitialAdapterAdMob.h"
+#import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+@interface YumiMediationInterstitialAdapterAdMob () <YumiMediationInterstitialAdapter>
+
+@property (nonatomic) YumiMediationInterstitialProvider *provider;
+@property (nonatomic, weak) id<YumiMediationInterstitialAdapterDelegate> delegate;
+@property (nonatomic) GADInterstitial *interstitial;
+
+@end
+
+@implementation YumiMediationInterstitialAdapterAdMob
+
++ (void)load {
+    [[YumiMediationAdapterRegistry registry] registerInterstitialAdapter:self
+                                                           forProviderID:@"10002"
+                                                             requestType:YumiMediationSDKAdRequest];
+}
+
+- (id<YumiMediationInterstitialAdapter>)initWithProvider:(YumiMediationInterstitialProvider *)provider
+                                                delegate:(id<YumiMediationInterstitialAdapterDelegate>)delegate {
+    self = [super init];
+
+    self.provider = provider;
+    self.delegate = delegate;
+
+    return self;
+}
+
+- (GADInterstitial *)interstitial {
+    if (!_interstitial) {
+        _interstitial = [[GADInterstitial alloc] initWithAdUnitID:self.provider.data.key1];
+        _interstitial.delegate = self;
+    }
+
+    return _interstitial;
+}
+
+- (void)requestAd {
+    GADRequest *request = [GADRequest request];
+    [self.interstitial loadRequest:request];
+}
+
+- (BOOL)isReady {
+    return [self.interstitial isReady];
+}
+
+- (void)presentFromRootViewController:(UIViewController *)rootViewController {
+    [self.interstitial presentFromRootViewController:rootViewController];
+}
+
+#pragma mark - GADInterstitialDelegate
+- (void)interstitialDidReceiveAd:(GADInterstitial *)ad {
+    [self.delegate adapter:self didReceiveInterstitialAd:ad];
+}
+
+- (void)interstitial:(GADInterstitial *)ad didFailToReceiveAdWithError:(GADRequestError *)error {
+    [self.delegate adapter:self interstitialAd:ad didFailToReceive:[error localizedDescription]];
+}
+
+- (void)interstitialWillPresentScreen:(GADInterstitial *)ad {
+}
+
+- (void)interstitialWillDismissScreen:(GADInterstitial *)ad {
+    [self.delegate adapter:self willDismissScreen:ad];
+}
+
+- (void)interstitialWillLeaveApplication:(GADInterstitial *)ad {
+    [self.delegate adapter:self didClickInterstitialAd:ad];
+}
+
+@end
