16b53c17e4cf66c6b013f69a0d0dd7af40d3234a
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-06-27 17:56:49 +0800

    code formate

--- YumiMediationAdapters/GDT/YumiMediationBannerAdapterNativeGDT.m
+++ YumiMediationAdapters/GDT/YumiMediationBannerAdapterNativeGDT.m
@@ -7,9 +7,9 @@
 //
 
 #import "YumiMediationBannerAdapterNativeGDT.h"
-#import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
-#import <YumiMediationSDK/YumiBannerViewTemplateManager.h>
 #import "GDTNativeAd.h"
+#import <YumiMediationSDK/YumiBannerViewTemplateManager.h>
+#import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
 
 @interface YumiMediationBannerAdapterNativeGDT () <YumiMediationBannerAdapter, GDTNativeAdDelegate, UIWebViewDelegate>
 
@@ -36,21 +36,21 @@
 #pragma mark :private method
 - (void)requestBannerViewAdTemplate {
     NSString *fileName = [NSString stringWithFormat:@"banner%@", self.provider.data.providerID];
-    
+
     self.templateManager =
-    [[YumiBannerViewTemplateManager alloc] initWithGeneralTemplate:self.provider.data.generalTemplate
-                                                 landscapeTemplate:self.provider.data.landscapeTemplate
-                                                  verticalTemplate:self.provider.data.verticalTemplate
-                                                  saveTemplateName:fileName];
-    
+        [[YumiBannerViewTemplateManager alloc] initWithGeneralTemplate:self.provider.data.generalTemplate
+                                                     landscapeTemplate:self.provider.data.landscapeTemplate
+                                                      verticalTemplate:self.provider.data.verticalTemplate
+                                                      saveTemplateName:fileName];
+
     __weak typeof(self) weakSelf = self;
     [self.templateManager fetchMediationTemplateSuccess:^(YumiMediationTemplateModel *_Nullable templateModel) {
         weakSelf.templateModel = templateModel;
     }
-                                                failure:^(NSError *_Nonnull error) {
-                                                    [[YumiLogger stdLogger] log:kLogLevelError message:[error localizedDescription]];
-                                                }];
-    
+        failure:^(NSError *_Nonnull error) {
+            [[YumiLogger stdLogger] log:kLogLevelError message:[error localizedDescription]];
+        }];
+
     self.currentID = [self.templateManager getCurrentNativeTemplate].templateID;
 }
 
@@ -58,27 +58,26 @@
     NSBundle *mainBundle = [NSBundle bundleForClass:[self class]];
     NSURL *bundleURL = [mainBundle URLForResource:@"YumiMediationSDK" withExtension:@"bundle"];
     NSBundle *YumiMediationSDK = [NSBundle bundleWithURL:bundleURL];
-    
+
     NSString *strPath = [YumiMediationSDK pathForResource:[NSString stringWithFormat:@"%@", name] ofType:@"html"];
     return strPath;
 }
 
 #pragma mark - YumiMediationBannerAdapter
-
 - (id<YumiMediationBannerAdapter>)initWithProvider:(YumiMediationBannerProvider *)provider
                                           delegate:(id<YumiMediationBannerAdapterDelegate>)delegate {
     self = [super init];
-    
+
     self.provider = provider;
     self.delegate = delegate;
-    
+
     return self;
 }
 
 - (void)requestAdWithIsPortrait:(BOOL)isPortrait isiPad:(BOOL)isiPad {
     // request remote template
     [self requestBannerViewAdTemplate];
-    
+
     CGSize adSize = isiPad ? CGSizeMake(728, 90) : CGSizeMake(320, 50);
     CGRect adframe = CGRectMake(0, 0, adSize.width, adSize.height);
     __weak typeof(self) weakSelf = self;
@@ -89,14 +88,14 @@
         }
         // webView init
         strongSelf.webView = [[UIWebView alloc] initWithFrame:adframe];
-        strongSelf.webView.delegate = self;
+        strongSelf.webView.delegate = strongSelf;
         strongSelf.webView.scrollView.scrollEnabled = NO;
         // nativeAD init
-        strongSelf.nativeAd =
-        [[GDTNativeAd alloc] initWithAppkey:strongSelf.provider.data.key1 placementId:self.provider.data.key2];
+        strongSelf.nativeAd = [[GDTNativeAd alloc] initWithAppkey:strongSelf.provider.data.key1
+                                                      placementId:strongSelf.provider.data.key2];
         strongSelf.nativeAd.controller = strongSelf.delegate.rootViewControllerForPresentingBannerView;
-        strongSelf.nativeAd.delegate = self;
-        
+        strongSelf.nativeAd.delegate = strongSelf;
+
         [strongSelf.nativeAd loadAd:1]; // The number of times a request has been requested “1”
     });
 }
@@ -108,32 +107,38 @@
     }
     self.data = nativeAdDataArray;
     self.currentAd = [self.data objectAtIndex:0];
-    
-    if (![_currentAd.properties objectForKey:GDTNativeAdDataKeyIconUrl] ||
-        ![_currentAd.properties objectForKey:GDTNativeAdDataKeyDesc]) {
+
+    if (![self.currentAd.properties objectForKey:GDTNativeAdDataKeyIconUrl] ||
+        ![self.currentAd.properties objectForKey:GDTNativeAdDataKeyDesc]) {
         [self.delegate adapter:self didFailToReceiveAd:@"GDT no ad"];
         return;
     }
-    
+
     NSString *path = [self resourceNamedFromCustomBundle:@"native-banner"];
     NSData *data = [NSData dataWithContentsOfFile:path];
     NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     str = [NSString stringWithFormat:str, @"100%", @"100%", @"50%", @"100%", @"100%", @"100%", @"100%", @"跳转",
-           [_currentAd.properties objectForKey:GDTNativeAdDataKeyIconUrl],
-           [_currentAd.properties objectForKey:GDTNativeAdDataKeyTitle],
-           [_currentAd.properties objectForKey:GDTNativeAdDataKeyDesc], @"%"];
+                                     [self.currentAd.properties objectForKey:GDTNativeAdDataKeyIconUrl],
+                                     [self.currentAd.properties objectForKey:GDTNativeAdDataKeyTitle],
+                                     [self.currentAd.properties objectForKey:GDTNativeAdDataKeyDesc], @"%"];
     if (self.templateModel) {
-        NSString *templateID = [NSString stringWithFormat:@"%d",self.templateModel.templateID];
+        NSString *templateID = [NSString stringWithFormat:@"%d", self.templateModel.templateID];
         NSString *currentID = [NSString stringWithFormat:@"%@", [NSNumber numberWithInteger:self.currentID]];
-        if (![templateID isEqualToString:currentID] ||  !self.templateModel.htmlString) {
+        if (![templateID isEqualToString:currentID] || !self.templateModel.htmlString) {
             [self.webView loadHTMLString:str baseURL:nil];
             return;
         }
-        
+
         str = self.templateModel.htmlString;
-        str = [self.templateManager replaceHtmlCharactersWithString:str iconURL:[_currentAd.properties objectForKey:GDTNativeAdDataKeyIconUrl] title:[_currentAd.properties objectForKey:GDTNativeAdDataKeyTitle] description:[_currentAd.properties objectForKey:GDTNativeAdDataKeyDesc] imageURL:@"大图" hyperlinkURL:@"跳转"];
+        str = [self.templateManager
+            replaceHtmlCharactersWithString:str
+                                    iconURL:[self.currentAd.properties objectForKey:GDTNativeAdDataKeyIconUrl]
+                                      title:[self.currentAd.properties objectForKey:GDTNativeAdDataKeyTitle]
+                                description:[self.currentAd.properties objectForKey:GDTNativeAdDataKeyDesc]
+                                   imageURL:@"大图"
+                               hyperlinkURL:@"跳转"];
     }
-    
+
     [self.webView loadHTMLString:str baseURL:nil];
 }
 
@@ -143,8 +148,8 @@
 
 #pragma mark : - UIWebViewDelegate
 - (BOOL)webView:(UIWebView *)webView
-shouldStartLoadWithRequest:(NSURLRequest *)request
- navigationType:(UIWebViewNavigationType)navigationType {
+    shouldStartLoadWithRequest:(NSURLRequest *)request
+                navigationType:(UIWebViewNavigationType)navigationType {
     if (navigationType == UIWebViewNavigationTypeLinkClicked) {
         [self.nativeAd clickAd:self.currentAd];
         [self.delegate adapter:self didClick:self.webView on:CGPointZero withTemplateID:(int)self.currentID];
@@ -154,7 +159,7 @@ shouldStartLoadWithRequest:(NSURLRequest *)request
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
-    [self.nativeAd attachAd:_currentAd toView:webView];
+    [self.nativeAd attachAd:self.currentAd toView:webView];
     [self.delegate adapter:self didReceiveAd:self.webView withTemplateID:(int)self.currentID];
 }
 
