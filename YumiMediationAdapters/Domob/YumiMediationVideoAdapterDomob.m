2cc2c070da9effcc89682a6531f588dc90540b6c
xiaoleiwei <weixiaolei@zplay.com>
2017-06-27 18:37:04 +0800

    implement Domob adapter

--- YumiMediationAdapters/Domob/YumiMediationVideoAdapterDomob.m
+++ YumiMediationAdapters/Domob/YumiMediationVideoAdapterDomob.m
@@ -7,8 +7,12 @@
 //
 
 #import "YumiMediationVideoAdapterDomob.h"
+#import "IndependentVideoManager.h"
 
-@interface YumiMediationVideoAdapterDomob ()
+@interface YumiMediationVideoAdapterDomob () <IndependentVideoManagerDelegate>
+    
+@property (nonatomic) IndependentVideoManager *videoManager;
+@property (nonatomic, assign) BOOL available;
 
 @end
 
@@ -35,23 +39,47 @@
                  delegate:(id<YumiMediationVideoAdapterDelegate>)delegate {
     self.delegate = delegate;
     self.provider = provider;
-
-    // TODO: setup code
+    
+    self.videoManager = [[IndependentVideoManager alloc] initWithPublisherID:self.provider.data.key1 andUserID:nil];
+    self.videoManager.delegate = self;
+    self.videoManager.openLogger = NO;
 }
 
 - (void)requestAd {
-    // TODO: request ad
+    [self.videoManager checkVideoAvailable];
 }
 
 - (BOOL)isReady {
-    // TODO: check if ready
-    return YES;
+	return self.available;
 }
 
 - (void)presentFromRootViewController:(UIViewController *)rootViewController {
-    // TODO: present video ad
+    [self.videoManager presentIndependentVideoWithViewController:rootViewController];
 }
 
-// TODO: implement third party sdk delegate and delegate to mediation sdk
+#pragma mark - IndependentVideoManagerDelegate
+- (void)ivManagerDidFinishLoad:(IndependentVideoManager *)manager finished:(BOOL)isFinished {
+    [self.delegate adapter:self didReceiveVideoAd:manager];
+}
+
+- (void)ivManager:(IndependentVideoManager *)manager failedLoadWithError:(NSError *)error {
+    [self.delegate adapter:self videoAd:manager didFailToLoad:[error localizedDescription]];
+}
+
+- (void)ivManagerWillPresent:(IndependentVideoManager *)manager {
+    [self.delegate adapter:self didOpenVideoAd:manager];
+}
+
+- (void)ivManagerDidClosed:(IndependentVideoManager *)manager {
+    self.available = NO;
+    
+    [self.delegate adapter:self didCloseVideoAd:manager];
+    
+    [self.delegate adapter:self videoAd:manager didReward:nil];
+}
+
+- (void)ivManager:(IndependentVideoManager *)manager isIndependentVideoAvailable:(BOOL)available {
+    self.available = available;
+}
 
 @end
