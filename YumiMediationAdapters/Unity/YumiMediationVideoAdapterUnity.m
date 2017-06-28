4afe69547f83eb73ee3b0c8bcb33d4c12be7ca5b
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-06-28 15:08:02 +0800

    Add  kYumiMediationAdapterIDUnity

--- YumiMediationAdapters/Unity/YumiMediationVideoAdapterUnity.m
+++ YumiMediationAdapters/Unity/YumiMediationVideoAdapterUnity.m
@@ -17,7 +17,7 @@
 
 + (void)load {
     [[YumiMediationAdapterRegistry registry] registerVideoAdapter:[self sharedInstance]
-                                                      forProvider:@"10019"
+                                                      forProvider:kYumiMediationAdapterIDUnity
                                                       requestType:YumiMediationSDKAdRequest];
 }
 
