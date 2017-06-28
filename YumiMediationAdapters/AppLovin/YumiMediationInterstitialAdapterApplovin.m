7c8f074d8bcfc28dbe170d08d3f34d61107fe458
weixiaolei <weixiaolei@zplay.com>
2017-06-28 17:46:06 +0800

    implement applovin interstitial adapter

---
+++ YumiMediationAdapters/AppLovin/YumiMediationInterstitialAdapterApplovin.m
@@ -0,0 +1,83 @@
+//
+//  YumiMediationInterstitialAdapterApplovin.m
+//  Pods
+//
+//  Created by generator on 28/06/2017.
+//
+//
+
+#import "YumiMediationInterstitialAdapterApplovin.h"
+#import <AppLovinSDK/ALInterstitialAd.h>
+
+
+@interface YumiMediationInterstitialAdapterApplovin () <ALAdLoadDelegate, ALAdDisplayDelegate>
+
+@property (nonatomic) ALInterstitialAd *interstitial;
+@property (nonatomic) ALAd *ad;
+@property (nonatomic) ALSdk *sdk;
+
+@end
+
+@implementation YumiMediationInterstitialAdapterApplovin
+
++ (void)load {
+    [[YumiMediationAdapterRegistry registry] registerInterstitialAdapter:self
+                                                           forProviderID:kYumiMediationAdapterIDAppLovin
+                                                             requestType:YumiMediationSDKAdRequest];
+}
+
+#pragma mark - YumiMediationInterstitialAdapter
+- (id<YumiMediationInterstitialAdapter>)initWithProvider:(YumiMediationInterstitialProvider *)provider
+                                                delegate:(id<YumiMediationInterstitialAdapterDelegate>)delegate {
+    self = [super init];
+
+    self.provider = provider;
+    self.delegate = delegate;
+
+    
+    self.sdk = [ALSdk sharedWithKey:self.provider.data.key1];
+    
+    self.interstitial = [[ALInterstitialAd alloc] initWithSdk:self.sdk];
+    self.interstitial.adLoadDelegate = self;
+    self.interstitial.adDisplayDelegate = self;
+
+    return self;
+}
+
+- (void)requestAd {
+    [[self.sdk adService] loadNextAd:[ALAdSize sizeInterstitial] andNotify:self];
+}
+
+- (BOOL)isReady {
+    return [ALInterstitialAd isReadyForDisplay];
+}
+
+- (void)presentFromRootViewController:(UIViewController *)rootViewController {
+    [self.interstitial showOver: [UIApplication sharedApplication].keyWindow andRender: self.ad];
+}
+
+#pragma mark - Ad Load Delegate
+- (void)adService:(nonnull ALAdService *)adService didLoadAd:(nonnull ALAd *)ad {
+    self.ad = ad;
+
+    [self.delegate adapter:self didReceiveInterstitialAd:ad];
+}
+
+- (void) adService:(nonnull ALAdService *)adService didFailToLoadAdWithError:(int)code {
+    [self.delegate adapter:self interstitialAd:nil didFailToReceive:[NSString stringWithFormat:@"applovin error code:%d",code]];
+}
+
+#pragma mark - Ad Display Delegate
+- (void)ad:(nonnull ALAd *)ad wasDisplayedIn:(nonnull UIView *)view {
+    [self.delegate adapter:self willPresentScreen:ad];
+}
+
+- (void)ad:(nonnull ALAd *)ad wasHiddenIn:(nonnull UIView *)view {
+    [self.delegate adapter:self willDismissScreen:ad];
+}
+
+- (void)ad:(nonnull ALAd *)ad wasClickedIn:(nonnull UIView *)view {
+    [self.delegate adapter:self didClickInterstitialAd:ad];
+}
+
+@end
