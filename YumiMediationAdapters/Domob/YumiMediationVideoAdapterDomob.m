cd499406fe5471b0cb6d750c2288557494675df9
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-06-28 15:07:23 +0800

    Add  kYumiMediationAdapterIDDomob

--- YumiMediationAdapters/Domob/YumiMediationVideoAdapterDomob.m
+++ YumiMediationAdapters/Domob/YumiMediationVideoAdapterDomob.m
@@ -20,7 +20,7 @@
 
 + (void)load {
     [[YumiMediationAdapterRegistry registry] registerVideoAdapter:[self sharedInstance]
-                                                      forProvider:@"10025"
+                                                      forProvider:kYumiMediationAdapterIDDomob
                                                       requestType:YumiMediationSDKAdRequest];
 }
 
