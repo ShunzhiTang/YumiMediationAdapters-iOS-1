0eb2059596216f5c3f5abc1d64e24b87489553b0
weixiaolei <weixiaolei@zplay.com>
2017-06-29 11:36:21 +0800

    implement AdMob interstitial adapter

---
+++ YumiMediationAdapters/AdMob/YumiMediationInterstitialAdapterAdMob.m
@@ -0,0 +1,75 @@
+//
+//  YumiMediationInterstitialAdapterAdMob.m
+//  Pods
+//
+//  Created by generator on 29/06/2017.
+//
+//
+
+#import "YumiMediationInterstitialAdapterAdMob.h"
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+@interface YumiMediationInterstitialAdapterAdMob () <GADInterstitialDelegate>
+
+@property (nonatomic) GADInterstitial *interstitial;
+
+@end
+
+
+@implementation YumiMediationInterstitialAdapterAdMob
+
++ (void)load {
+    [[YumiMediationAdapterRegistry registry] registerInterstitialAdapter:self
+                                                           forProviderID:kYumiMediationAdapterIDAdMob
+                                                             requestType:YumiMediationSDKAdRequest];
+}
+
+
+#pragma mark - YumiMediationInterstitialAdapter
+- (id<YumiMediationInterstitialAdapter>)initWithProvider:(YumiMediationInterstitialProvider *)provider
+                                                delegate:(id<YumiMediationInterstitialAdapterDelegate>)delegate {
+    self = [super init];
+    
+    self.provider = provider;
+    self.delegate = delegate;
+    
+    self.interstitial = [[GADInterstitial alloc] initWithAdUnitID:self.provider.data.key1];
+    self.interstitial.delegate = self;
+    
+    return self;
+}
+
+- (void)requestAd {
+    GADRequest *request = [GADRequest request];
+    [self.interstitial loadRequest:request];
+}
+
+- (BOOL)isReady {
+    return [self.interstitial isReady];
+}
+
+- (void)presentFromRootViewController:(UIViewController *)rootViewController {
+    [self.interstitial presentFromRootViewController:rootViewController];
+}
+
+#pragma mark - GADInterstitialDelegate
+- (void)interstitialDidReceiveAd:(GADInterstitial *)ad {
+    [self.delegate adapter:self didReceiveInterstitialAd:ad];
+}
+
+- (void)interstitial:(GADInterstitial *)ad didFailToReceiveAdWithError:(GADRequestError *)error {
+    [self.delegate adapter:self interstitialAd:ad didFailToReceive:[error localizedDescription]];
+}
+
+- (void)interstitialWillPresentScreen:(GADInterstitial *)ad {
+}
+
+- (void)interstitialWillDismissScreen:(GADInterstitial *)ad {
+    [self.delegate adapter:self willDismissScreen:ad];
+}
+
+- (void)interstitialWillLeaveApplication:(GADInterstitial *)ad {
+    [self.delegate adapter:self didClickInterstitialAd:ad];
+}
+
+@end
