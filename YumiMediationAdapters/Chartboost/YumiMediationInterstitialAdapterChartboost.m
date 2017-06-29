315ff5c4306d8cc39e09b52e868ca7b9e74b697a
weixiaolei <weixiaolei@zplay.com>
2017-06-29 15:29:52 +0800

    implement Chartboost interstitial adapter

---
+++ YumiMediationAdapters/Chartboost/YumiMediationInterstitialAdapterChartboost.m
@@ -0,0 +1,74 @@
+//
+//  YumiMediationInterstitialAdapterChartboost.m
+//  Pods
+//
+//  Created by generator on 29/06/2017.
+//
+//
+
+#import "YumiMediationInterstitialAdapterChartboost.h"
+#import <Chartboost/Chartboost.h>
+
+@interface YumiMediationInterstitialAdapterChartboost () <ChartboostDelegate>
+
+@end
+
+@implementation YumiMediationInterstitialAdapterChartboost
+
++ (void)load {
+    [[YumiMediationAdapterRegistry registry] registerInterstitialAdapter:self
+                                                           forProviderID:kYumiMediationAdapterIDChartboost
+                                                             requestType:YumiMediationSDKAdRequest];
+}
+
+#pragma mark - YumiMediationInterstitialAdapter
+- (id<YumiMediationInterstitialAdapter>)initWithProvider:(YumiMediationInterstitialProvider *)provider
+                                                delegate:(id<YumiMediationInterstitialAdapterDelegate>)delegate {
+    self = [super init];
+
+    self.provider = provider;
+    self.delegate = delegate;
+
+    [Chartboost startWithAppId:self.provider.data.key1 appSignature:self.provider.data.key2 delegate:self];
+
+    return self;
+}
+
+- (void)requestAd {
+    [Chartboost cacheInterstitial:CBLocationHomeScreen];
+}
+
+- (BOOL)isReady {
+    return [Chartboost hasInterstitial:CBLocationHomeScreen];
+}
+
+- (void)presentFromRootViewController:(UIViewController *)rootViewController {
+    [Chartboost showInterstitial:CBLocationHomeScreen];
+}
+
+#pragma mark - ChartboostDelegate
+- (void)didCacheInterstitial:(NSString *)location {
+    [self.delegate adapter:self didReceiveInterstitialAd:nil];
+}
+
+- (void)didFailToLoadInterstitial:(NSString *)location withError:(CBLoadError)error {
+    [self.delegate adapter:self
+            interstitialAd:nil
+          didFailToReceive:[NSString stringWithFormat:@"Chartboost error code: %d", error]];
+}
+
+- (BOOL)shouldDisplayInterstitial:(NSString *)location {
+    [self.delegate adapter:self willPresentScreen:nil];
+
+    return YES;
+}
+
+- (void)didDismissInterstitial:(NSString *)location {
+    [self.delegate adapter:self willDismissScreen:nil];
+}
+
+- (void)didClickInterstitial:(CBLocation)location {
+    [self.delegate adapter:self didClickInterstitialAd:nil];
+}
+
+@end
