7835039f63fb88fc8b78faecec17e9a072b7ca0b
weixiaolei <weixiaolei@zplay.com>
2017-06-29 17:48:03 +0800

    implement GDT inerstitial adapter

---
+++ YumiMediationAdapters/GDT/YumiMediationInterstitialAdapterGDT.m
@@ -0,0 +1,74 @@
+//
+//  YumiMediationInterstitialAdapterGDT.m
+//  Pods
+//
+//  Created by generator on 29/06/2017.
+//
+//
+
+#import "YumiMediationInterstitialAdapterGDT.h"
+#import "GDTMobInterstitial.h"
+
+@interface YumiMediationInterstitialAdapterGDT () <GDTMobInterstitialDelegate>
+
+@property (nonatomic) GDTMobInterstitial *interstitial;
+
+@end
+
+@implementation YumiMediationInterstitialAdapterGDT
+
++ (void)load {
+    [[YumiMediationAdapterRegistry registry] registerInterstitialAdapter:self
+                                                           forProviderID:kYumiMediationAdapterIDGDT
+                                                             requestType:YumiMediationSDKAdRequest];
+}
+
+#pragma mark - YumiMediationInterstitialAdapter
+- (id<YumiMediationInterstitialAdapter>)initWithProvider:(YumiMediationInterstitialProvider *)provider
+                                                delegate:(id<YumiMediationInterstitialAdapterDelegate>)delegate {
+    self = [super init];
+
+    self.provider = provider;
+    self.delegate = delegate;
+
+    self.interstitial =
+        [[GDTMobInterstitial alloc] initWithAppkey:self.provider.data.key1 placementId:self.provider.data.key2];
+    self.interstitial.delegate = self;
+
+    return self;
+}
+
+- (void)requestAd {
+    [self.interstitial loadAd];
+}
+
+- (BOOL)isReady {
+    return [self.interstitial isReady];
+}
+
+- (void)presentFromRootViewController:(UIViewController *)rootViewController {
+    [self.interstitial presentFromRootViewController:rootViewController];
+}
+
+#pragma mark - GDTMobInterstitialDelegate
+- (void)interstitialSuccessToLoadAd:(GDTMobInterstitial *)interstitial {
+    [self.delegate adapter:self didReceiveInterstitialAd:interstitial];
+}
+
+- (void)interstitialFailToLoadAd:(GDTMobInterstitial *)interstitial error:(NSError *)error {
+    [self.delegate adapter:self interstitialAd:interstitial didFailToReceive:[error localizedDescription]];
+}
+
+- (void)interstitialWillPresentScreen:(GDTMobInterstitial *)interstitial {
+    [self.delegate adapter:self willPresentScreen:interstitial];
+}
+
+- (void)interstitialDidDismissScreen:(GDTMobInterstitial *)interstitial {
+    [self.delegate adapter:self willDismissScreen:interstitial];
+}
+
+- (void)interstitialClicked:(GDTMobInterstitial *)interstitial {
+    [self.delegate adapter:self didClickInterstitialAd:interstitial];
+}
+
+@end
