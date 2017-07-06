c4dd31db92400fdb34448e86b7f0ff2050db436e
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-07-06 10:46:51 +0800

    Add admob interstitial native

---
+++ YumiMediationAdapters/AdMob/YumiMediationInterstitialAdapterNativeAdMob.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationInterstitialAdapterNativeAdMob.h
+//  Pods
+//
+//  Created by ShunZhi Tang on 2017/7/6.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationInterstitialAdapterNativeAdMob : NSObject
+
+@end
---
+++ YumiMediationAdapters/AdMob/YumiMediationInterstitialAdapterNativeAdMob.m
@@ -0,0 +1,13 @@
+//
+//  YumiMediationInterstitialAdapterNativeAdMob.m
+//  Pods
+//
+//  Created by ShunZhi Tang on 2017/7/6.
+//
+//
+
+#import "YumiMediationInterstitialAdapterNativeAdMob.h"
+
+@implementation YumiMediationInterstitialAdapterNativeAdMob
+
+@end
