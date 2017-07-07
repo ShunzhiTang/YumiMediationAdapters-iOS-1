7c4235ac10bf4d8f0d792fec73899b4ef23866a2
weixiaolei <weixiaolei@zplay.com>
2017-07-07 20:12:17 +0800

    code format

--- YumiMediationAdapters/GDT/YumiMediationStreamAdapterGDT.m
+++ YumiMediationAdapters/GDT/YumiMediationStreamAdapterGDT.m
@@ -10,7 +10,7 @@
 #import "GDTNativeAd.h"
 #import "YUMIStreamModel.h"
 
-@interface YumiMediationStreamAdapterGDT() <GDTNativeAdDelegate>
+@interface YumiMediationStreamAdapterGDT () <GDTNativeAdDelegate>
 
 @property (nonatomic) GDTNativeAd *nativeAd;
 @property (nonatomic) NSArray *data;
@@ -21,7 +21,7 @@
 
 @implementation YumiMediationStreamAdapterGDT
 
-+(NSString *)StreamRegister {
++ (NSString *)StreamRegister {
     return YuMIStreamNetworkAdGDT;
 }
 
@@ -30,11 +30,11 @@
 }
 
 - (void)getStream:(NSString *)numbers {
-    
+
     [self startStream];
-    
-    self.isReading=NO;
-    
+
+    self.isReading = NO;
+
     NSTimeInterval interval = [self.streamProvider.outTime doubleValue];
     self.timer = [NSTimer scheduledTimerWithTimeInterval:interval
                                                   target:self
@@ -42,26 +42,24 @@
                                                 userInfo:nil
                                                  repeats:NO];
 
-    
     self.nativeAd = [[GDTNativeAd alloc] initWithAppkey:self.streamProvider.key1 placementId:self.streamProvider.key2];
     self.nativeAd.controller = [self viewControllerForPresentStream];
     self.nativeAd.delegate = self;
     [self.nativeAd loadAd:[numbers intValue]];
-    
 }
 
 - (void)stopStream {
-    
 }
 
--(void)timeOutTimer{
+- (void)timeOutTimer {
     if (self.isReading) {
         return;
     }
     [self stopTimer];
-    self.isReading=YES;
-    
-    [self adapter:self didFailedToReceiveStreamWithError:[NSError errorWithDomain:@"GDT stream time out" code:8 userInfo:nil]];
+    self.isReading = YES;
+
+    [self adapter:self
+        didFailedToReceiveStreamWithError:[NSError errorWithDomain:@"GDT stream time out" code:8 userInfo:nil]];
 }
 
 - (void)stopTimer {
@@ -72,24 +70,25 @@
 }
 
 #pragma mark - GDTNativeAdDelegate
--(void)nativeAdSuccessToLoad:(NSArray *)nativeAdDataArray {
-    
+- (void)nativeAdSuccessToLoad:(NSArray *)nativeAdDataArray {
+
     if (self.isReading) {
         return;
     }
     [self stopTimer];
-    self.isReading=YES;
-    
-    if(!nativeAdDataArray || ![nativeAdDataArray objectAtIndex:0]){
-        [self adapter:self didFailedToReceiveStreamWithError:[NSError errorWithDomain:@"GDT stream no ad" code:8 userInfo:nil]];
+    self.isReading = YES;
+
+    if (!nativeAdDataArray || ![nativeAdDataArray objectAtIndex:0]) {
+        [self adapter:self
+            didFailedToReceiveStreamWithError:[NSError errorWithDomain:@"GDT stream no ad" code:8 userInfo:nil]];
         return;
     }
-    
+
     NSMutableArray *modelArray = [[NSMutableArray alloc] init];
-    for (int i=0; i<nativeAdDataArray.count; i++) {
-        
+    for (int i = 0; i < nativeAdDataArray.count; i++) {
+
         GDTNativeAdData *data = nativeAdDataArray[i];
-        
+
         YUMIStreamModel *model = [[YUMIStreamModel alloc] init];
         model.title = data.properties[GDTNativeAdDataKeyTitle];
         model.desc = data.properties[GDTNativeAdDataKeyDesc];
@@ -101,35 +100,33 @@
         model.adapter = self;
         model.data = data;
         model.providerId = YuMIStreamNetworkAdGDT;
-        model.pid = [NSString stringWithFormat:@"%@_%d",[[YUMIStreamDevice shareDevice] getYUMIPid],i];
+        model.pid = [NSString stringWithFormat:@"%@_%d", [[YUMIStreamDevice shareDevice] getYUMIPid], i];
         [modelArray addObject:model];
     }
-    
+
     [self adapter:self didReceiveStreamData:modelArray didReceiveStreamWithNumbers:(int)(modelArray.count)];
-    
 }
 
--(void)nativeAdFailToLoad:(NSError *)error{
+- (void)nativeAdFailToLoad:(NSError *)error {
     if (self.isReading) {
         return;
     }
-    self.isReading=YES;
+    self.isReading = YES;
     [self stopTimer];
-    
-    [self adapter:self didFailedToReceiveStreamWithError:[NSError errorWithDomain:@"GDT stream no ad" code:8 userInfo:nil]];
-}
 
+    [self adapter:self
+        didFailedToReceiveStreamWithError:[NSError errorWithDomain:@"GDT stream no ad" code:8 userInfo:nil]];
+}
 
 - (void)showStream:(YUMIStreamModel *)streamModel view:(UIView *)view {
     [super showStream:streamModel view:view];
-    
+
     [_nativeAd attachAd:streamModel.data toView:view];
 }
 
 - (void)clickStream:(YUMIStreamModel *)streamModel {
     [super clickStream:streamModel];
-    
-    
+
     [_nativeAd clickAd:streamModel.data];
 }
 
