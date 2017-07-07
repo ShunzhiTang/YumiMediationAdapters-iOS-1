591ae61a4e147e544d2fd8ec4c796d608f74eada
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-07-07 14:34:40 +0800

    Request interstitial ad template

--- YumiMediationAdapters/GDT/YumiMediationInterstitialAdapterNativeGDT.m
+++ YumiMediationAdapters/GDT/YumiMediationInterstitialAdapterNativeGDT.m
@@ -34,6 +34,35 @@
                                                              requestType:YumiMediationSDKAdRequest];
 }
 
+#pragma mark :private method
+- (void)requestInterstitialAdTemplate {
+    NSString *fileName = [NSString stringWithFormat:@"inter%@", self.provider.data.providerID];
+    
+    self.templateManager =
+    [[YumiBannerViewTemplateManager alloc] initWithGeneralTemplate:self.provider.data.generalTemplate
+                                                 landscapeTemplate:self.provider.data.landscapeTemplate
+                                                  verticalTemplate:self.provider.data.verticalTemplate
+                                                  saveTemplateName:fileName];
+    
+    __weak typeof(self) weakSelf = self;
+    [self.templateManager fetchMediationTemplateSuccess:^(YumiMediationTemplateModel *_Nullable templateModel) {
+        weakSelf.templateModel = templateModel;
+    }
+                                                failure:^(NSError *_Nonnull error) {
+                                                    [[YumiLogger stdLogger] log:kLogLevelError message:[error localizedDescription]];
+                                                }];
+    
+    self.currentID = [self.templateManager getCurrentNativeTemplate].templateID;
+}
+
+- (NSString *)resourceNamedFromCustomBundle:(NSString *)name {
+    NSBundle *YumiMediationSDK = [[YumiTool sharedTool] resourcesBundleWithBundleName:@"YumiMediationSDK"];
+    NSString *strPath = [YumiMediationSDK pathForResource:[NSString stringWithFormat:@"%@", name] ofType:@"html"];
+    
+    return strPath;
+}
+
+
 #pragma mark - YumiMediationInterstitialAdapter
 - (id<YumiMediationInterstitialAdapter>)initWithProvider:(YumiMediationInterstitialProvider *)provider
                                                 delegate:(id<YumiMediationInterstitialAdapterDelegate>)delegate {
