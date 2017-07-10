065113512c0e2d1ee7e0f4fafd081ab04ca7dfa0
weixiaolei <weixiaolei@zplay.com>
2017-07-10 14:39:37 +0800

    add yumi stream adapter

---
+++ YumiMediationAdapters/AdNative/YumiMediationStreamAdapterYumi.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationStreamAdapterYumi.h
+//  Pods
+//
+//  Created by 魏晓磊 on 2017/7/10.
+//
+//
+
+#import <YumiMediationSDK/YUMIStreamNetworkAdapter.h>
+
+@interface YumiMediationStreamAdapterYumi : YUMIStreamNetworkAdapter
+
+@end
