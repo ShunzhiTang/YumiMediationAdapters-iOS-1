10d6c4b1266b18737982358a3c7887399723c061
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-07-21 18:21:32 +0800

    Add facebook native  interstitial adapter

---
+++ YumiMediationAdapters/Facebook/YumiMediationInterstitialAdapterNativeFacebook.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationInterstitialAdapterNativeFacebook.h
+//  Pods
+//
+//  Created by ShunZhi Tang on 2017/7/21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationInterstitialAdapterNativeFacebook : NSObject
+
+@end
---
+++ YumiMediationAdapters/Facebook/YumiMediationInterstitialAdapterNativeFacebook.m
@@ -0,0 +1,13 @@
+//
+//  YumiMediationInterstitialAdapterNativeFacebook.m
+//  Pods
+//
+//  Created by ShunZhi Tang on 2017/7/21.
+//
+//
+
+#import "YumiMediationInterstitialAdapterNativeFacebook.h"
+
+@implementation YumiMediationInterstitialAdapterNativeFacebook
+
+@end
