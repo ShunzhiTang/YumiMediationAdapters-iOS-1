5f6a544f1589f443ada73dfb194776fee9757d8c
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-08-03 18:18:31 +0800

    Add YumiMediationVideoAdapterMobvista

---
+++ YumiMediationAdapters/Mobvista/YumiMediationVideoAdapterMobvista.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationVideoAdapterMobvista.h
+//  Pods
+//
+//  Created by ShunZhi Tang on 2017/8/3.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationVideoAdapterMobvista : NSObject
+
+@end
---
+++ YumiMediationAdapters/Mobvista/YumiMediationVideoAdapterMobvista.m
@@ -0,0 +1,13 @@
+//
+//  YumiMediationVideoAdapterMobvista.m
+//  Pods
+//
+//  Created by ShunZhi Tang on 2017/8/3.
+//
+//
+
+#import "YumiMediationVideoAdapterMobvista.h"
+
+@implementation YumiMediationVideoAdapterMobvista
+
+@end
