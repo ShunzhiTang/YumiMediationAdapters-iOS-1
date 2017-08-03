429182d0f6cb6b00f1347f35089b948c570a3e85
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-08-03 18:51:15 +0800

    Implement mobvista video adapter

--- YumiMediationAdapters/Mobvista/YumiMediationVideoAdapterMobvista.m
+++ YumiMediationAdapters/Mobvista/YumiMediationVideoAdapterMobvista.m
@@ -9,10 +9,11 @@
 #import "YumiMediationVideoAdapterMobvista.h"
 #import <MVSDK/MVSDK.h>
 #import <MVSDKReward/MVRewardAdManager.h>
-//#import <MVSDKReward/MVRewardAdInfo.h>
 
+@interface YumiMediationVideoAdapterMobvista ()<MVRewardAdLoadDelegate , MVRewardAdShowDelegate>
 
-@interface YumiMediationVideoAdapterMobvista ()
+@property (assign) BOOL isAdReady;
+@property (nonatomic) MVRewardAdManager  *videoAd;
 
 @end
 
@@ -32,6 +33,61 @@
     });
     
     return sharedInstance;
+}
+
+#pragma mark: YumiMediationVideoAdapter
+- (void)setupWithProvider:(YumiMediationVideoProvider *)provider delegate:(id<YumiMediationVideoAdapterDelegate>)delegate{
+    
+    self.provider = provider;
+    self.delegate = delegate;
+    self.videoAd = [MVRewardAdManager sharedInstance];
+}
+
+- (void)requestAd{
+    
+   [self.videoAd loadVideo:self.provider.data.key1 delegate:self];
+}
+
+- (void)presentFromRootViewController:(UIViewController *)rootViewController{
+    [self.videoAd showVideo:self.provider.data.key1 withRewardId:self.provider.data.key2 userId:self.provider.data.key3 ?: @"" delegate:self viewController:rootViewController];
+}
+
+- (BOOL)isReady{
+    return self.isAdReady;
+}
+
+#pragma mark: - MVRewardAdLoadDelegate
+
+- (void)onVideoAdLoadSuccess:(nullable NSString *)unitId
+{
+    self.isAdReady = YES;
+    [self.delegate adapter:self didReceiveVideoAd:self.videoAd];
+}
+- (void)onVideoAdLoadFailed:(nullable NSString *)unitId error:(nonnull NSError *)error
+{
+    self.isAdReady = NO;
+    [self.delegate adapter:self videoAd:self.videoAd didFailToLoad:[error localizedDescription]];
+}
+
+#pragma mark: - MVRewardAdShowDelegate
+- (void)onVideoAdShowSuccess:(nullable NSString *)unitId
+{
+    [self.delegate adapter:self didOpenVideoAd:self.videoAd];
+}
+- (void)onVideoAdShowFailed:(nullable NSString *)unitId withError:(nonnull NSError *)error
+{
+    [self.delegate adapter:self videoAd:self.videoAd didFailToLoad:[error localizedDescription]];
+}
+- (void)onVideoAdClicked:(nullable NSString *)unitId
+{
+    [self.delegate adapter:self didCloseVideoAd:self.videoAd];
+}
+- (void)onVideoAdDismissed:(NSString *)unitId withConverted:(BOOL)converted withRewardInfo:(MVRewardAdInfo *)rewardInfo
+{
+    if (rewardInfo) {
+        
+        [self.delegate adapter:self videoAd:self.videoAd didReward:rewardInfo];
+    }
 }
 
 @end
