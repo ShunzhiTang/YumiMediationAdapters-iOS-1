c0d765e27639ca68e94eff71b49f04bb6286544d
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-08-29 17:56:04 +0800

    Implement inmobi native adapter

--- YumiMediationAdapters/InMobi/YumiMediationInterstitialAdapterNativeInMobi.m
+++ YumiMediationAdapters/InMobi/YumiMediationInterstitialAdapterNativeInMobi.m
@@ -7,9 +7,19 @@
 
 #import "YumiMediationInterstitialAdapterNativeInMobi.h"
 #import <YumiMediationSDK/YumiTool.h>
+#import <YumiMediationSDK/YumiAdsCustomViewController.h>
+#import <YumiMediationSDK/YumiBannerViewTemplateManager.h>
+#import <InMobiSDK/InMobiSDK.h>
 
-@interface YumiMediationInterstitialAdapterNativeInMobi ()
+@interface YumiMediationInterstitialAdapterNativeInMobi ()<IMNativeDelegate,YumiAdsCustomViewControllerDelegate>
 
+@property (nonatomic) IMNative *imnative;
+@property (nonatomic) NSDictionary *imobeDict;
+@property (nonatomic) YumiAdsCustomViewController *interstitial;
+
+@property (nonatomic) YumiMediationTemplateModel *templateModel;
+@property (nonatomic, assign) NSInteger currentID;
+@property (nonatomic) YumiBannerViewTemplateManager *templateManager;
 
 @end
 
@@ -22,7 +32,34 @@
 }
 
 #pragma mark :private method
+- (NSString *)resourceNamedFromCustomBundle:(NSString *)name {
+    NSBundle *YumiMediationSDK = [[YumiTool sharedTool] resourcesBundleWithBundleName:@"YumiMediationSDK"];
+    NSString *strPath = [YumiMediationSDK pathForResource:[NSString stringWithFormat:@"%@", name] ofType:@"html"];
+    
+    return strPath;
+}
 
+- (void)createCustomViewControllerWith:(CGFloat)width height:(CGFloat)height{
+    CGRect inmobiFrame = CGRectMake(0, 0, width, height);
+    
+    NSDictionary *closeBtnFrame = @{
+                                    @"closeButton_w" : @(self.provider.data.closeButton.clickAreaWidth),
+                                    @"closeButton_h" : @(self.provider.data.closeButton.clickAreaHeight),
+                                    @"closeImage_w" : @(self.provider.data.closeButton.pictureWidth),
+                                    @"closeImage_h" : @(self.provider.data.closeButton.pictureHeight)
+                                    };
+    
+   
+        self.interstitial = [[YumiAdsCustomViewController alloc]
+                                 initYumiAdsCustomViewControllerWith:inmobiFrame
+                                 clickType:YumiAdsClickTypeOpenSystem
+                                 closeBtnPosition:self.provider.data.closeButton.position
+                                 closeBtnFrame:closeBtnFrame
+                                 logoType:YumiAdsLogoCommon
+                                 delegate:self];
+        self.interstitial.isNativeInterstitialGDT = NO;
+    
+}
 
 #pragma mark - YumiMediationInterstitialAdapter
 - (id<YumiMediationInterstitialAdapter>)initWithProvider:(YumiMediationInterstitialProvider *)provider
@@ -31,23 +68,123 @@
 
     self.provider = provider;
     self.delegate = delegate;
-
+    
+    [IMSdk initWithAccountID:self.provider.data.key1];
+    [IMSdk setLogLevel:kIMSDKLogLevelNone];
+    
     return self;
 }
 
 - (void)requestAd {
-
+    
+    __weak typeof(self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        weakSelf.imnative = [[IMNative alloc] initWithPlacementId:[self.provider.data.key2 longLongValue]];
+       weakSelf.imnative.delegate = self;
+        [weakSelf.imnative load];
+        
+        });
+    
 }
 
 - (BOOL)isReady {
     
+    return  self.interstitial.isReady;
 }
 
 - (void)present {
+     [self.interstitial presentFromRootViewController:[self.delegate rootViewControllerForPresentingModalView]];
     
+    // inmobi present
+    [IMNative bindNative:self.imnative toView:self.interstitial.customView];
 }
 
-#pragma mark : - 
+#pragma mark : - IMNativeDelegate
 
+-(void)nativeDidFinishLoading:(IMNative*)native{
+    
+    if (!native || !native.adContent) {
+        [self.delegate adapter:self interstitialAd:self.interstitial didFailToReceive:@"Inmobi native no ad"];
+        return;
+    }
+    
+    if (native.adContent != nil) {
+        NSData *data = [native.adContent dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error = nil;
+        self.imobeDict = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+    }
+    if (!self.imobeDict || ![self.imobeDict objectForKey:@"screenshots"]) {
+        [self.delegate adapter:self interstitialAd:self.interstitial didFailToReceive:@"Inmobi native no ad"];
+        return;
+    }
+    
+    NSUInteger width = [[[self.imobeDict objectForKey:@"screenshots"] objectForKey:@"width"] integerValue];
+    NSUInteger height = [[[self.imobeDict objectForKey:@"screenshots"] objectForKey:@"height"] integerValue];
+    NSString *url = [[self.imobeDict objectForKey:@"screenshots"] objectForKey:@"url"];
+    NSString *clickUrl = [self.imobeDict objectForKey:@"landingURL"] ;
+    
+    if (!url || url.length == 0 || width == 0 || height == 0 || !clickUrl || clickUrl.length == 0) {
+       [self.delegate adapter:self interstitialAd:self.interstitial didFailToReceive:@"Inmobi native no ad"];
+        return;
+    }
+    
+    [self createCustomViewControllerWith:width height:height];
+    
+    NSString *interstitialPath = [self resourceNamedFromCustomBundle:@"cp-img"];
+    
+    NSData *interstitialData = [NSData dataWithContentsOfFile:interstitialPath];
+    NSString *interstitialStr = [[NSString alloc] initWithData:interstitialData encoding:NSUTF8StringEncoding];
+    
+    interstitialStr = [NSString stringWithFormat:interstitialStr, @"100%", @"100%", @"100%", @"100%",
+                       clickUrl, url];
+    
+    [self.interstitial loadHTMLString:interstitialStr];
+}
+
+-(void)native:(IMNative*)native didFailToLoadWithError:(IMRequestStatus*)error{
+      [self.delegate adapter:self interstitialAd:self.interstitial didFailToReceive:[error localizedDescription]];
+}
+
+-(void)nativeWillPresentScreen:(IMNative*)native{
+}
+-(void)nativeDidPresentScreen:(IMNative*)native{
+}
+-(void)nativeWillDismissScreen:(IMNative*)native{
+}
+-(void)nativeDidDismissScreen:(IMNative*)native{
+}
+-(void)userWillLeaveApplicationFromNative:(IMNative*)native{
+}
+-(void)nativeAdImpressed:(IMNative*)native{
+}
+
+#pragma mark : YumiAdsCustomViewControllerDelegate
+- (void)yumiAdsCustomViewControllerDidReceivedAd:(UIViewController *)viewController {
+    
+    [self.delegate adapter:self
+  didReceiveInterstitialAd:self.interstitial
+         interstitialFrame:CGRectZero
+            withTemplateID:(int)self.currentID];
+}
+
+- (void)yumiAdsCustomViewController:(UIViewController *)viewController didFailToReceiveAdWithError:(NSError *)error {
+    [self.delegate adapter:self interstitialAd:self.interstitial didFailToReceive:[error localizedDescription]];
+}
+
+- (void)didClickOnYumiAdsCustomViewController:(UIViewController *)viewController point:(CGPoint)point {
+    // inmobi点击
+    [self.imnative reportAdClick:self.imobeDict];
+    
+    [self.delegate adapter:self didClickInterstitialAd:self.interstitial on:point withTemplateID:(int)self.currentID];
+}
+
+- (void)yumiAdsCustomViewControllerDidPresent:(UIViewController *)viewController {
+    
+    [self.delegate adapter:self willPresentScreen:self.interstitial];
+}
+
+- (void)yumiAdsCustomViewControllerDidClosed:(UIViewController *)viewController {
+    [self.delegate adapter:self willDismissScreen:self.interstitial];
+}
 
 @end
