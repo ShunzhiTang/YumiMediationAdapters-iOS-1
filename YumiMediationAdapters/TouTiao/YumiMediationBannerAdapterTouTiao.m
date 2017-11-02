67d82c1068d318b862137881fb6751fc96e3bcfb
ShunZhi Tang <tangshunzhi@zplay.com>
2017-11-02 15:04:40 +0800

    Code format

--- YumiMediationAdapters/TouTiao/YumiMediationBannerAdapterTouTiao.m
+++ YumiMediationAdapters/TouTiao/YumiMediationBannerAdapterTouTiao.m
@@ -7,13 +7,13 @@
 //
 
 #import "YumiMediationBannerAdapterTouTiao.h"
-#import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
-#import <WMAdSDK/WMBannerAdView.h>
 #import <WMAdSDK/WMAdSDKManager.h>
-#import <YumiMediationSDK/YumiTool.h>
+#import <WMAdSDK/WMBannerAdView.h>
+#import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
 #import <YumiMediationSDK/YumiMediationConstants.h>
+#import <YumiMediationSDK/YumiTool.h>
 
-@interface YumiMediationBannerAdapterTouTiao () <YumiMediationBannerAdapter,WMBannerAdViewDelegate>
+@interface YumiMediationBannerAdapterTouTiao () <YumiMediationBannerAdapter, WMBannerAdViewDelegate>
 
 @property (nonatomic, weak) id<YumiMediationBannerAdapterDelegate> delegate;
 @property (nonatomic) YumiMediationBannerProvider *provider;
@@ -43,39 +43,42 @@
 }
 
 - (void)requestAdWithIsPortrait:(BOOL)isPortrait isiPad:(BOOL)isiPad {
-    
-    WMSize *ttSize = isiPad ?  [WMSize sizeBy:WMProposalSize_Banner600_300] :  [WMSize sizeBy:WMProposalSize_Banner600_100];
+
+    WMSize *ttSize =
+        isiPad ? [WMSize sizeBy:WMProposalSize_Banner600_300] : [WMSize sizeBy:WMProposalSize_Banner600_100];
 
     CGSize adSize = isiPad ? CGSizeMake(728, 90) : CGSizeMake(320, 50);
     if ([[[NSUserDefaults standardUserDefaults] objectForKey:autoAdSize] boolValue]) {
-        
-         adSize = [[YumiTool sharedTool] fetchBannerAdSize];
+
+        adSize = [[YumiTool sharedTool] fetchBannerAdSize];
     }
-    CGRect  adFrame = CGRectMake(0, 0, adSize.width , adSize.height);
-    
+    CGRect adFrame = CGRectMake(0, 0, adSize.width, adSize.height);
+
     __weak typeof(self) weakSelf = self;
     dispatch_async(dispatch_get_main_queue(), ^{
-        
-        weakSelf.bannerView = [[WMBannerAdView alloc] initWithSlotID:weakSelf.provider.data.key2 size:ttSize rootViewController:weakSelf.delegate.rootViewControllerForPresentingModalView];
+
+        weakSelf.bannerView =
+            [[WMBannerAdView alloc] initWithSlotID:weakSelf.provider.data.key2
+                                              size:ttSize
+                                rootViewController:weakSelf.delegate.rootViewControllerForPresentingModalView];
         weakSelf.bannerView.delegate = weakSelf;
         weakSelf.bannerView.frame = adFrame;
         weakSelf.bannerView.dislikeButton.hidden = YES;
-        
+
         [weakSelf.bannerView loadAdData];
     });
-    
 }
 
-#pragma mark:- WMBannerAdViewDelegate
-- (void)bannerAdViewDidLoad:(WMBannerAdView *)bannerAdView WithAdmodel:(WMNativeAd *_Nullable)nativeAd{
+#pragma mark :- WMBannerAdViewDelegate
+- (void)bannerAdViewDidLoad:(WMBannerAdView *)bannerAdView WithAdmodel:(WMNativeAd *_Nullable)nativeAd {
     [self.delegate adapter:self didReceiveAd:bannerAdView];
 }
 
-- (void)bannerAdViewDidClick:(WMBannerAdView *)bannerAdView WithAdmodel:(WMNativeAd *_Nullable)nativeAd{
+- (void)bannerAdViewDidClick:(WMBannerAdView *)bannerAdView WithAdmodel:(WMNativeAd *_Nullable)nativeAd {
     [self.delegate adapter:self didClick:bannerAdView];
 }
 
-- (void)bannerAdView:(WMBannerAdView *)bannerAdView didLoadFailWithError:(NSError *_Nullable)error{
+- (void)bannerAdView:(WMBannerAdView *)bannerAdView didLoadFailWithError:(NSError *_Nullable)error {
     [self.delegate adapter:self didFailToReceiveAd:[error localizedDescription]];
 }
 
