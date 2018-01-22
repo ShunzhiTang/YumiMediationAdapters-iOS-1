dde5d060ee5bc63e1ffb073229afbc3b84598d69
ShunZhi Tang <tangshunzhi@zplay.com>
2018-01-22 15:24:06 +0800

    Add PlayableAds interstitial adapter

---
+++ YumiMediationAdapters/PlayableAds/YumiMediationInterstitialAdapterPlayableAds.h
@@ -0,0 +1,17 @@
+//
+//  YumiMediationInterstitialAdapterPlayableAds.h
+//  Pods
+//
+//  Created by generator on 22/01/2018.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
+
+@interface YumiMediationInterstitialAdapterPlayableAds : NSObject <YumiMediationInterstitialAdapter>
+
+@property (nonatomic, weak) id<YumiMediationInterstitialAdapterDelegate> delegate;
+@property (nonatomic) YumiMediationInterstitialProvider *provider;
+
+@end
