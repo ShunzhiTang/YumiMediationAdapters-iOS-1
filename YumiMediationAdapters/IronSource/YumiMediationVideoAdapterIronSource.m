d5ad3270206b10d2da4796ecd682fcf295a66c5d
JiaDingYi <wzy2010416033@163.com>
2018-08-10 15:54:30 +0800

    finetune code comments

--- YumiMediationAdapters/IronSource/YumiMediationVideoAdapterIronSource.m
+++ YumiMediationAdapters/IronSource/YumiMediationVideoAdapterIronSource.m
@@ -36,7 +36,7 @@
 }
 
 - (void)requestAd {
-    // NOTE: Unity do not provide any method for requesting ad, it handles the request internally
+    // NOTE: ironsource do not provide any method for requesting ad, it handles the request internally
 }
 
 - (BOOL)isReady {
@@ -48,27 +48,32 @@
 }
 
 #pragma mark - ISRewardedVideoDelegate
-
+//Called after a rewarded video has changed its availability.
+//@param available The new rewarded video availability. YES if available //and ready to be shown, NO otherwise.
 - (void)rewardedVideoHasChangedAvailability:(BOOL)available {
     if (available) {
         [self.delegate adapter:self didReceiveVideoAd:nil];
     }
 }
 
+//Called after a rewarded video has been viewed completely and the user is //eligible for reward.@param placementInfo An object that contains the //placement's reward name and amount.
 - (void)didReceiveRewardForPlacement:(ISPlacementInfo *)placementInfo {
     self.isReward = YES;
 }
 
+//Called after a rewarded video has attempted to show but failed.
+//@param error The reason for the error
 - (void)rewardedVideoDidFailToShowWithError:(NSError *)error {
     [self.delegate adapter:self videoAd:nil didFailToLoad:[error localizedDescription]];
 }
 
+//Called after a rewarded video has been opened.
 - (void)rewardedVideoDidOpen {
     [self.delegate adapter:self didOpenVideoAd:nil];
 }
 
+//Called after a rewarded video has been dismissed.
 - (void)rewardedVideoDidClose {
-
     if (self.isReward) {
         [self.delegate adapter:self videoAd:nil didReward:nil];
         self.isReward = NO;
@@ -76,14 +81,20 @@
     [self.delegate adapter:self didCloseVideoAd:nil];
 }
 
+//Note: the events below are not available for all supported rewarded video ad networks. Check which events are available per ad network you choose //to include in your build.
+//We recommend only using events which register to ALL ad networks you //include in your build.
+//Called after a rewarded video has started playing.
 - (void)rewardedVideoDidStart {
     [self.delegate adapter:self didStartPlayingVideoAd:nil];
 }
 
+//Called after a rewarded video has finished playing.
 - (void)rewardedVideoDidEnd {
 }
 
+/**
+ Called after a video has been clicked.
+ */
 - (void)didClickRewardedVideo:(ISPlacementInfo *)placementInfo {
 }
-
 @end
