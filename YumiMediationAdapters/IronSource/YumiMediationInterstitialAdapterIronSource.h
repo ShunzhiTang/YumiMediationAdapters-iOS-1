f319ce8be5170cb58f805ce97a2e71e3c4851d7e
wangzeyong <wzy2010416033@163.com>
2018-08-10 14:45:16 +0800

    create ironsource interstitial adapter

---
+++ YumiMediationAdapters/IronSource/YumiMediationInterstitialAdapterIronSource.h
@@ -0,0 +1,12 @@
+//
+//  YumiMediationInterstitialAdapterIronSource.h
+//  Pods-YumiMediationSDK-iOS_Example
+//
+//  Created by 王泽永 on 2018/8/10.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationInterstitialAdapterIronSource : NSObject
+
+@end
