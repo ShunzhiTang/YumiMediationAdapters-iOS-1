ac43e21e68d95d6f42abc46feef708536b2d2bf3
JiaDingYi <wzy2010416033@163.com>
2018-08-20 21:00:36 +0800

    implement ISDemandOnlyInterstitial

--- YumiMediationAdapters/IronSource/YumiMediationInterstitialAdapterIronSource.m
+++ YumiMediationAdapters/IronSource/YumiMediationInterstitialAdapterIronSource.m
@@ -8,7 +8,7 @@
 #import "YumiMediationInterstitialAdapterIronSource.h"
 #import <IronSource/IronSource.h>
 
-@interface YumiMediationInterstitialAdapterIronSource () <ISInterstitialDelegate>
+@interface YumiMediationInterstitialAdapterIronSource () <ISDemandOnlyInterstitialDelegate>
 @end
 
 @implementation YumiMediationInterstitialAdapterIronSource
@@ -27,13 +27,13 @@
     self.provider = provider;
     self.delegate = delegate;
 
-    [IronSource setInterstitialDelegate:self];
+    [IronSource setISDemandOnlyInterstitialDelegate:self];
     if (self.provider.data.key1.length == 0) {
         [self.delegate adapter:self interstitialAd:nil didFailToReceive:@"No app id specified"];
         return self;
     }
-    [IronSource initWithAppKey:self.provider.data.key1 adUnits:@[ IS_INTERSTITIAL ]];
-
+    [IronSource initISDemandOnly:self.provider.data.key1 adUnits:@[IS_INTERSTITIAL]];
+    [IronSource loadISDemandOnlyInterstitial:self.provider.data.key2];
     return self;
 }
 
@@ -42,64 +42,62 @@
 }
 
 - (BOOL)isReady {
-    return [IronSource hasInterstitial];
+    return [IronSource hasISDemandOnlyInterstitial:self.provider.data.key2];
     ;
 }
 
 - (void)present {
-    [IronSource showInterstitialWithViewController:[self.delegate rootViewControllerForPresentingModalView]];
+    [IronSource showISDemandOnlyInterstitial:[self.delegate rootViewControllerForPresentingModalView] instanceId:self.provider.data.key2];
 }
 
-#pragma mark - ISInterstitialDelegate
+#pragma mark - ISDemandOnlyInterstitialDelegate
 /**
  Called after an interstitial has been loaded
  */
-- (void)interstitialDidLoad {
+- (void)interstitialDidLoad:(NSString *)instanceId{
     [self.delegate adapter:self didReceiveInterstitialAd:nil];
 }
 
 /**
  Called after an interstitial has attempted to load but failed.
-
  @param error The reason for the error
  */
-- (void)interstitialDidFailToLoadWithError:(NSError *)error {
+- (void)interstitialDidFailToLoadWithError:(NSError *)error instanceId:(NSString *)instanceId{
     [self.delegate adapter:self interstitialAd:nil didFailToReceive:[error localizedDescription]];
 }
 
 /**
  Called after an interstitial has been opened.
  */
-- (void)interstitialDidOpen {
+- (void)interstitialDidOpen:(NSString *)instanceId{
     [self.delegate adapter:self willPresentScreen:nil];
 }
 
 /**
  Called after an interstitial has been dismissed.
  */
-- (void)interstitialDidClose {
+- (void)interstitialDidClose:(NSString *)instanceId{
     [self.delegate adapter:self willDismissScreen:nil];
 }
 
 /**
  Called after an interstitial has been displayed on the screen.
  */
-- (void)interstitialDidShow {
+- (void)interstitialDidShow:(NSString *)instanceId{
 }
 
 /**
  Called after an interstitial has attempted to show but failed.
-
  @param error The reason for the error
  */
-- (void)interstitialDidFailToShowWithError:(NSError *)error {
+- (void)interstitialDidFailToShowWithError:(NSError *)error instanceId:(NSString *)instanceId{
     [self.delegate adapter:self interstitialAd:nil didFailToReceive:[error localizedDescription]];
 }
 
 /**
  Called after an interstitial has been clicked.
  */
-- (void)didClickInterstitial {
+- (void)didClickInterstitial:(NSString *)instanceId{
     [self.delegate adapter:self didClickInterstitialAd:nil];
 }
 
