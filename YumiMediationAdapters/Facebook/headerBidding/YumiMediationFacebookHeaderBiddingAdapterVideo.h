56a56407ea2c59bd0b374be52e2247b70fc2a25b
JiaDingYi <wzy2010416033@163.com>
2018-09-05 11:02:39 +0800

    create facebook header bidding video adapter

---
+++ YumiMediationAdapters/Facebook/headerBidding/YumiMediationFacebookHeaderBiddingAdapterVideo.h
@@ -0,0 +1,12 @@
+//
+//  YumiMediationFacebookHeaderBiddingAdapterVideo.h
+//  Pods-YumiMediationSDK-iOS_Example
+//
+//  Created by 王泽永 on 2018/9/5.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationFacebookHeaderBiddingAdapterVideo : NSObject
+
+@end
---
+++ YumiMediationAdapters/Facebook/headerBidding/YumiMediationFacebookHeaderBiddingAdapterVideo.m
@@ -0,0 +1,12 @@
+//
+//  YumiMediationFacebookHeaderBiddingAdapterVideo.m
+//  Pods-YumiMediationSDK-iOS_Example
+//
+//  Created by 王泽永 on 2018/9/5.
+//
+
+#import "YumiMediationFacebookHeaderBiddingAdapterVideo.h"
+
+@implementation YumiMediationFacebookHeaderBiddingAdapterVideo
+
+@end
