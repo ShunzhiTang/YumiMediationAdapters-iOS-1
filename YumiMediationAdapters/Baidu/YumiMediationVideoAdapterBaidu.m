b91847187d9e075c30267640531a13be6bae9cbc
ShunZhi Tang <tangshunzhi@zplay.com>
2018-09-26 16:46:48 +0800

    Add baidu video adapter

---
+++ YumiMediationAdapters/Baidu/YumiMediationVideoAdapterBaidu.m
@@ -0,0 +1,83 @@
+//
+//  YumiMediationVideoAdapterBaidu.m
+//  Pods
+//
+//  Created by generator on 26/09/2018.
+//
+//
+
+#import "YumiMediationVideoAdapterBaidu.h"
+#import <BaiduMobAdSDK/BaiduMobAdRewardVideo.h>
+#import <BaiduMobAdSDK/BaiduMobAdSetting.h>
+
+@interface YumiMediationVideoAdapterBaidu () <BaiduMobAdRewardVideoDelegate>
+
+@property (nonatomic) BaiduMobAdRewardVideo *rewardVideo;
+@property (nonatomic, assign) BOOL isReward;
+
+@end
+
+@implementation YumiMediationVideoAdapterBaidu
+
++ (void)load {
+    [[YumiMediationAdapterRegistry registry] registerVideoAdapter:self
+                                                      forProvider:kYumiMediationAdapterIDBaidu
+                                                      requestType:YumiMediationSDKAdRequest];
+}
+
+#pragma mark - YumiMediationVideoAdapter
+- (id<YumiMediationVideoAdapter>)initWithProvider:(YumiMediationVideoProvider *)provider
+                                         delegate:(id<YumiMediationVideoAdapterDelegate>)delegate {
+    self.delegate = delegate;
+    self.provider = provider;
+
+    self.rewardVideo = [[BaiduMobAdRewardVideo alloc] init];
+
+    self.rewardVideo.delegate = self;
+    self.rewardVideo.publisherId = self.provider.data.key1;
+    self.rewardVideo.AdUnitTag = self.provider.data.key2;
+
+    return self;
+}
+
+- (void)requestAd {
+    [self.rewardVideo preload];
+}
+
+- (BOOL)isReady {
+
+    return [self.rewardVideo isReady];
+}
+
+- (void)presentFromRootViewController:(UIViewController *)rootViewController {
+    [self.rewardVideo loadAndDisplayWithViewController:rootViewController];
+}
+
+#pragma mark :BaiduMobAdRewardVideoDelegate
+- (void)videoPreloadSuccess:(BaiduMobAdRewardVideo *)video {
+    [self.delegate adapter:self didReceiveVideoAd:video];
+}
+
+- (void)videoPreloadFail:(BaiduMobAdRewardVideo *)video withError:(BaiduMobFailReason)reason {
+    self.isReward = NO;
+    [self.delegate adapter:self videoAd:video didFailToLoad:[NSString stringWithFormat:@"%u", reason] isRetry:YES];
+}
+
+- (void)videoFailPresentScreen:(BaiduMobAdRewardVideo *)video withError:(BaiduMobFailReason)reason {
+    self.isReward = NO;
+    [self.delegate adapter:self videoAd:video didFailToLoad:[NSString stringWithFormat:@"%u", reason] isRetry:YES];
+}
+
+- (void)videoDidFinishPlayingMedia:(BaiduMobAdRewardVideo *)video {
+    self.isReward = YES;
+}
+
+- (void)userDidSkipPlayingMedia:(BaiduMobAdRewardVideo *)video withPlayingProgress:(CGFloat)progress {
+    if (self.isReward) {
+        [self.delegate adapter:self videoAd:video didReward:nil];
+        self.isReward = NO;
+    }
+    [self.delegate adapter:self didCloseVideoAd:video];
+}
+
+@end
