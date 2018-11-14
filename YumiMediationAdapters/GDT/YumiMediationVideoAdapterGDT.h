ad3c1edc0e9aeb9f171038288d80f67936707e18
JiaDingYi <wzy2010416033@163.com>
2018-11-14 14:52:12 +0800

    create gdt video adapter

---
+++ YumiMediationAdapters/GDT/YumiMediationVideoAdapterGDT.h
@@ -0,0 +1,16 @@
+//
+//  YumiMediationVideoAdapterGDT.h
+//  Pods
+//
+//  Created by 王泽永 on 2018/11/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YumiMediationVideoAdapterGDT : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YumiMediationAdapters/GDT/YumiMediationVideoAdapterGDT.m
@@ -0,0 +1,12 @@
+//
+//  YumiMediationVideoAdapterGDT.m
+//  Pods
+//
+//  Created by 王泽永 on 2018/11/14.
+//
+
+#import "YumiMediationVideoAdapterGDT.h"
+
+@implementation YumiMediationVideoAdapterGDT
+
+@end
