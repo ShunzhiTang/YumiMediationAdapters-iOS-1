de46c679c2354bda0952c8ee7c7046bd090d5bfd
JiaDingYi <wzy2010416033@163.com>
2018-12-04 16:07:44 +0800

    remove vungle interstitial adapters when close ad

--- YumiMediationAdapters/Vungle/YumiMediationVungleInstance.m
+++ YumiMediationAdapters/Vungle/YumiMediationVungleInstance.m
@@ -76,6 +76,7 @@
     for (YumiMediationInterstitialAdapterVungle *interstitialAdapter in self.vungleInterstitialAdapters) {
         if ([interstitialAdapter.provider.data.key3 isEqualToString:placementID]) {
             [interstitialAdapter.delegate adapter:interstitialAdapter willDismissScreen:nil];
+            self.vungleInterstitialAdapters = nil;
             if ([info.didDownload boolValue]) {
                 [interstitialAdapter.delegate adapter:interstitialAdapter didClickInterstitialAd:nil];
             }
