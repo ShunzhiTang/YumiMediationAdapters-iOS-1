5e54a75ef2d2bea25a1da6173be2d56a5e4e2cfe
ShunZhi Tang <tangshunzhi@zplay.com>
2018-12-26 12:22:56 +0800

    Implement IQzone video adapter

--- YumiMediationAdapters/IQzone/YumiMediationVideoAdapterIQzone.m
+++ YumiMediationAdapters/IQzone/YumiMediationVideoAdapterIQzone.m
@@ -7,8 +7,15 @@
 //
 
 #import "YumiMediationVideoAdapterIQzone.h"
+#import <IMDInterstitialViewController.h>
+#import <IMDSDK.h>
+#import <YumiMediationSDK/YumiTool.h>
 
-@interface YumiMediationVideoAdapterIQzone ()
+@interface YumiMediationVideoAdapterIQzone ()<IMDRewardedViewDelegate>
+
+@property (nonatomic) IMDInterstitialViewController *rewardedVideo;
+@property (nonatomic , assign) BOOL isVideoReady;
+@property (nonatomic, assign) BOOL isReward;
 
 @end
 
@@ -28,23 +35,75 @@
     self.provider = provider;
     self.delegate = delegate;
     
+    self.rewardedVideo = [IMDSDK newRewardedInterstitialViewController:[[YumiTool sharedTool] topMostController] placementID:self.provider.data.key1 loadedListener:self andMetadata:nil];;
+    
     return self;
 }
 
 - (void)requestAd {
-    // TODO: request ad
+    self.isVideoReady = NO;
+    [self.rewardedVideo load];
 }
 
 - (BOOL)isReady {
-    // TODO: check if ready
-    return YES;
+    
+    return self.isVideoReady;
 }
 
 - (void)presentFromRootViewController:(UIViewController *)rootViewController {
-    // TODO: present video ad
+    
+    [self.rewardedVideo show: rootViewController];
 }
 
+#pragma mark:IMDRewardedViewDelegate
 
-// TODO: implement third party sdk delegate and delegate to mediation sdk
+- (void)adLoaded {
+    self.isVideoReady = YES;
+    
+    [self.delegate adapter:self didReceiveVideoAd:self.rewardedVideo];
+}
+
+- (void)adFailedToLoad {
+    self.isVideoReady = NO;
+    [self.delegate adapter:self videoAd:self.rewardedVideo didFailToLoad:@"video load failed"];
+}
+
+- (void)adImpression {
+
+    [self.delegate adapter:self didOpenVideoAd:self.rewardedVideo];
+    
+}
+
+- (void)adClicked {
+    
+}
+
+- (void)adDismissed {
+    if (self.isReward) {
+        [self.delegate adapter:self videoAd:self.rewardedVideo didReward:nil];
+        self.isReward = NO;
+    }
+    [self.delegate adapter:self didCloseVideoAd:self.rewardedVideo];
+}
+
+- (void)adExpanded {
+    
+}
+
+- (void)videoCompleted {
+    self.isReward = YES;
+}
+
+- (void)videoSkipped {
+    self.isReward = NO;
+}
+
+- (void)videoStarted {
+    [self.delegate adapter:self didStartPlayingVideoAd:self.rewardedVideo];
+}
+
+- (void)videoTrackerFired {
+    
+}
 
 @end
