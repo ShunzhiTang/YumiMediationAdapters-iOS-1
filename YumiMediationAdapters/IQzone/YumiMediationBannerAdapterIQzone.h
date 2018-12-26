d9932c7384833dbef459f3de613d13d23838b0f0
ShunZhi Tang <tangshunzhi@zplay.com>
2018-12-26 10:26:52 +0800

    Create IQzone banner adapter

---
+++ YumiMediationAdapters/IQzone/YumiMediationBannerAdapterIQzone.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationBannerAdapterIQzone.h
+//  Pods
+//
+//  Created by generator on 26/12/2018.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationBannerAdapterIQzone : NSObject
+
+@end
