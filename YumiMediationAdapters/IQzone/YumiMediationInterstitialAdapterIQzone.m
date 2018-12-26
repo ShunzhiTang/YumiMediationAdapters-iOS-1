a1c7c2657efa581ac26431f8fb89aa7706375948
ShunZhi Tang <tangshunzhi@zplay.com>
2018-12-26 11:33:36 +0800

    Implement IQzone interstitial adapter

--- YumiMediationAdapters/IQzone/YumiMediationInterstitialAdapterIQzone.m
+++ YumiMediationAdapters/IQzone/YumiMediationInterstitialAdapterIQzone.m
@@ -7,8 +7,13 @@
 //
 
 #import "YumiMediationInterstitialAdapterIQzone.h"
+#import <IMDSDK.h>
+#import <IMDInterstitialViewController.h>
 
-@interface YumiMediationInterstitialAdapterIQzone ()
+@interface YumiMediationInterstitialAdapterIQzone ()<IMDInterstitialViewDelegate>
+
+@property (nonatomic) IMDInterstitialViewController *interstitial;
+@property (nonatomic , assign) BOOL isInterstitialReady;
 
 @end
 
@@ -29,24 +34,68 @@
     self.delegate = delegate;
 
     // TODO: setup code
-
+    self.interstitial = [IMDSDK newInterstitialViewController:[self.delegate rootViewControllerForPresentingModalView] placementID:self.provider.data.key1 loadedListener:self andMetadata:nil];
+    
     return self;
 }
 
 - (void)requestAd {
-    // TODO: request ad
+    self.isInterstitialReady = NO;
+    [self.interstitial load];
 }
 
 - (BOOL)isReady {
-    // TODO: check if ready
-    return YES;
+    
+    return self.isInterstitialReady;
 }
 
 - (void)present {
-    UIViewController *rootViewController = [self.delegate rootViewControllerForPresentingModalView];
-    // TODO: present interstitial ad with rootViewController
+   
+    [self.interstitial show:[self.delegate rootViewControllerForPresentingModalView]];
 }
 
-// TODO: implement third party sdk delegate and delegate to mediation sdk
+#pragma mark: -IMDInterstitialViewDelegate
+
+- (void)adLoaded {
+    self.isInterstitialReady = YES;
+    
+    [self.delegate adapter:self didReceiveInterstitialAd:self.interstitial];
+}
+
+- (void)adClicked {
+    [self.delegate adapter:self didClickInterstitialAd:self.interstitial];
+}
+- (void)adFailedToLoad {
+    self.isInterstitialReady = NO;
+    [self.delegate adapter:self interstitialAd:self.interstitial didFailToReceive:@"interstitial load fail"];
+}
+
+- (void)adImpression {
+    [self.delegate adapter:self willPresentScreen:self.interstitial];
+}
+
+- (void)adDismissed {
+    [self.delegate adapter:self willDismissScreen:self.interstitial];
+}
+
+- (void)adExpanded {
+    
+}
+
+- (void)videoCompleted {
+    
+}
+
+- (void)videoSkipped {
+    
+}
+
+- (void)videoStarted {
+    
+}
+
+- (void)videoTrackerFired {
+    
+}
 
 @end
