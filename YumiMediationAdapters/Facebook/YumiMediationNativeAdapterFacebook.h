a4a156ab75f418b733b35a13112227fc3d6a28bb
ShunZhi Tang <tangshunzhi@zplay.com>
2019-02-13 15:01:51 +0800

    Generator facebook native adapter

---
+++ YumiMediationAdapters/Facebook/YumiMediationNativeAdapterFacebook.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationNativeAdapterFacebook.h
+//  Pods
+//
+//  Created by generator on 13/02/2019.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationNativeAdapterFacebook : NSObject
+
+@end
