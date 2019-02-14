239b446cadd76cd14283b999b3ed0f92c2642527
ShunZhi Tang <tangshunzhi@zplay.com>
2019-02-14 15:10:53 +0800

    Generator baidu native adapter

---
+++ YumiMediationAdapters/Baidu/YumiMediationNativeAdapterBaidu.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationNativeAdapterBaidu.h
+//  Pods
+//
+//  Created by generator on 14/02/2019.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationNativeAdapterBaidu : NSObject
+
+@end
