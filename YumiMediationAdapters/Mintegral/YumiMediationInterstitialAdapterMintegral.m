8dfb8afb0c189ce73506f84222e915eb6acd0dcb
JiaDingYi <wzy2010416033@163.com>
2019-02-28 17:34:51 +0800

    add new interstitial adapter

---
+++ YumiMediationAdapters/Mintegral/YumiMediationInterstitialAdapterMintegral.m
@@ -0,0 +1,12 @@
+//
+//  YumiMediationInterstitialAdapterMintegral.m
+//  Pods-YumiMediationSDK-iOS_Example
+//
+//  Created by 王泽永 on 2019/2/28.
+//
+
+#import "YumiMediationInterstitialAdapterMintegral.h"
+
+@implementation YumiMediationInterstitialAdapterMintegral
+
+@end
---
+++ YumiMediationAdapters/Mintegral/YumiMediationinterstitialVideoAdapterMintegral.h
@@ -0,0 +1,16 @@
+//
+//  YumiMediationinterstitialVideoAdapterMintegral.h
+//  Pods-YumiMediationSDK-iOS_Example
+//
+//  Created by 王泽永 on 2019/2/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YumiMediationinterstitialVideoAdapterMintegral : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
