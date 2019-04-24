416c3c5efc96be9592af6af5525967c4e476c700
JiaDingYi <wzy2010416033@163.com>
2019-04-24 21:53:08 +0800

    update fb interstitial adapter

--- YumiMediationAdapters/Facebook/interstitial/YumiMediationInterstitialAdapterFacebook.m
+++ YumiMediationAdapters/Facebook/interstitial/YumiMediationInterstitialAdapterFacebook.m
@@ -12,24 +12,28 @@
 @interface YumiMediationInterstitialAdapterFacebook () <FBInterstitialAdDelegate>
 
 @property (nonatomic) FBInterstitialAd *interstitial;
+@property (nonatomic, assign) YumiMediationAdType adType;
 
 @end
 
 @implementation YumiMediationInterstitialAdapterFacebook
 
 + (void)load {
-    [[YumiMediationAdapterRegistry registry] registerInterstitialAdapter:self
-                                                           forProviderID:kYumiMediationAdapterIDFacebook
-                                                             requestType:YumiMediationSDKAdRequest];
+    [[YumiMediationAdapterRegistry registry] registerCoreAdapter:self
+                                                   forProviderID:kYumiMediationAdapterIDFacebook
+                                                     requestType:YumiMediationSDKAdRequest
+                                                          adType:YumiMediationAdTypeInterstitial];
 }
 
 #pragma mark - YumiMediationInterstitialAdapter
-- (id<YumiMediationInterstitialAdapter>)initWithProvider:(YumiMediationInterstitialProvider *)provider
-                                                delegate:(id<YumiMediationInterstitialAdapterDelegate>)delegate {
+- (id<YumiMediationCoreAdapter>)initWithProvider:(YumiMediationCoreProvider *)provider
+                                                delegate:(id<YumiMediationCoreAdapterDelegate>)delegate
+                                          adType:(YumiMediationAdType)adType{
     self = [super init];
 
     self.provider = provider;
     self.delegate = delegate;
+    self.adType = adType;
 
     self.interstitial = [[FBInterstitialAd alloc] initWithPlacementID:self.provider.data.key1];
     self.interstitial.delegate = self;
@@ -45,25 +49,34 @@
     return self.interstitial.adValid;
 }
 
-- (void)present {
-    [self.interstitial showAdFromRootViewController:[self.delegate rootViewControllerForPresentingModalView]];
+- (void)presentFromRootViewController:(UIViewController *)rootViewController {
+    [self.interstitial showAdFromRootViewController:rootViewController];
 }
 
 #pragma mark - FBInterstitialAdDelegate
 - (void)interstitialAdDidLoad:(FBInterstitialAd *)interstitialAd {
-    [self.delegate adapter:self didReceiveInterstitialAd:interstitialAd];
+    [self.delegate coreAdapter:self didReceivedCoreAd:interstitialAd adType:self.adType];
 }
 
 - (void)interstitialAd:(FBInterstitialAd *)interstitialAd didFailWithError:(NSError *)error {
-    [self.delegate adapter:self interstitialAd:interstitialAd didFailToReceive:[error localizedDescription]];
+    [self.delegate coreAdapter:self coreAd:interstitialAd didFailToLoad:[error localizedDescription] adType:self.adType];
 }
 
 - (void)interstitialAdDidClick:(FBInterstitialAd *)interstitialAd {
-    [self.delegate adapter:self didClickInterstitialAd:interstitialAd];
+    [self.delegate coreAdapter:self didClickCoreAd:interstitialAd adType:self.adType];
 }
 
 - (void)interstitialAdDidClose:(FBInterstitialAd *)interstitialAd {
-    [self.delegate adapter:self willDismissScreen:interstitialAd];
+    [self.delegate coreAdapter:self didCloseCoreAd:interstitialAd isCompletePlaying:NO adType:self.adType];
 }
 
+/**
+ Sent immediately before the impression of an FBInterstitialAd object will be logged.
+ 
+ @param interstitialAd An FBInterstitialAd object sending the message.
+ */
+- (void)interstitialAdWillLogImpression:(FBInterstitialAd *)interstitialAd {
+    [self.delegate coreAdapter:self didOpenCoreAd:interstitialAd adType:self.adType];
+    [self.delegate coreAdapter:self didStartPlayingAd:interstitialAd adType:self.adType];
+}
 @end
