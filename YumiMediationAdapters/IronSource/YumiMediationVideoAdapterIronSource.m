699b3201b3fa6d6a8d971497c9cbc487876a8ff2
ShunZhi Tang <tangshunzhi@zplay.com>
2019-04-24 10:58:08 +0800

    Video isReward is YES

--- YumiMediationAdapters/IronSource/YumiMediationVideoAdapterIronSource.m
+++ YumiMediationAdapters/IronSource/YumiMediationVideoAdapterIronSource.m
@@ -68,7 +68,7 @@
 //@param placementInfo An object that contains the placement's reward name and amount.
 - (void)didReceiveRewardForPlacement:(ISPlacementInfo *)placementInfo instanceId:(NSString *)instanceId {
     self.isReward = YES;
-    [self.delegate coreAdapter:self coreAd:nil didReward:self.isReward adType:self.adType];
+    [self.delegate coreAdapter:self coreAd:nil didReward:YES adType:self.adType];
 }
 
 // Called after a rewarded video has attempted to show but failed.
@@ -86,7 +86,7 @@
 // Called after a rewarded video has been dismissed.
 - (void)rewardedVideoDidClose:(NSString *)instanceId {
     if (!self.isReward) { // ironsource 确保无中途关闭并且奖励回调始终在关闭之前
-        [self.delegate coreAdapter:self coreAd:nil didReward:self.isReward adType:self.adType];
+        [self.delegate coreAdapter:self coreAd:nil didReward:YES adType:self.adType];
     }
     
     [self.delegate coreAdapter:self didCloseCoreAd:nil isCompletePlaying:self.isReward adType:self.adType];
