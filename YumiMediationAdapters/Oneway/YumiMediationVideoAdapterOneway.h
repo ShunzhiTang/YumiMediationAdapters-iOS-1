0c60e29f048018c4b5b33265c5a0836c6334570c
ShunZhi Tang <tangshunzhi@zplay.com>
2019-04-25 09:49:57 +0800

    OneWay implement video adapter

--- YumiMediationAdapters/Oneway/YumiMediationVideoAdapterOneway.h
+++ YumiMediationAdapters/Oneway/YumiMediationVideoAdapterOneway.h
@@ -9,9 +9,9 @@
 #import <Foundation/Foundation.h>
 #import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
 
-@interface YumiMediationVideoAdapterOneway : NSObject <YumiMediationVideoAdapter>
+@interface YumiMediationVideoAdapterOneway : NSObject <YumiMediationCoreAdapter>
 
-@property (nonatomic, weak) id<YumiMediationVideoAdapterDelegate> delegate;
-@property (nonatomic) YumiMediationVideoProvider *provider;
+@property (nonatomic, weak) id<YumiMediationCoreAdapterDelegate> delegate;
+@property (nonatomic) YumiMediationCoreProvider *provider;
 
 @end
