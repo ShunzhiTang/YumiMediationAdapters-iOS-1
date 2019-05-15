9c9c6b0ae1db5787e29bed9fd9e7f3ca53a8a868
JiaDingYi <wzy2010416033@163.com>
2019-05-15 19:06:11 +0800

    add gdpr adamob

---
+++ YumiMediationAdapters/AdMob/GDPR/YumiMediationGDPRAdmob.h
@@ -0,0 +1,17 @@
+//
+//  YumiMediationGDPRAdmob.h
+//  Pods
+//
+//  Created by 王泽永 on 2019/5/15.
+//
+
+#import <Foundation/Foundation.h>
+#import <YumiMediationSDK/YumiMediationGDPRAdapter.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YumiMediationGDPRAdmob : NSObject <YumiMediationGDPRAdapter>
+
+@end
+
+NS_ASSUME_NONNULL_END
