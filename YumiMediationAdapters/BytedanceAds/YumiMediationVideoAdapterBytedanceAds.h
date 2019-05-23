c813c58e05905c2441d1251615d41a06b1a55f09
ShunZhi Tang <tangshunzhi@zplay.com>
2019-05-23 12:03:26 +0800

    Implement BytedanceAds video adapter

---
+++ YumiMediationAdapters/BytedanceAds/YumiMediationVideoAdapterBytedanceAds.h
@@ -0,0 +1,17 @@
+//
+//  YumiMediationVideoAdapterBytedanceAds.h
+//  Pods
+//
+//  Created by generator on 23/05/2019.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
+
+@interface YumiMediationVideoAdapterBytedanceAds : NSObject <YumiMediationCoreAdapter>
+
+@property (nonatomic, weak) id<YumiMediationCoreAdapterDelegate> delegate;
+@property (nonatomic) YumiMediationCoreProvider *provider;
+
+@end
