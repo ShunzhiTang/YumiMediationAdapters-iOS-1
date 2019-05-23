78b576cd8a4bc713d454a4235bbef1fe98d7b4a1
ShunZhi Tang <tangshunzhi@zplay.com>
2019-05-23 11:22:04 +0800

    Implement BytedanceAds interstitial adapter

---
+++ YumiMediationAdapters/BytedanceAds/YumiMediationInterstitialAdapterBytedanceAds.h
@@ -0,0 +1,17 @@
+//
+//  YumiMediationInterstitialAdapterBytedanceAds.h
+//  Pods
+//
+//  Created by generator on 23/05/2019.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
+
+@interface YumiMediationInterstitialAdapterBytedanceAds : NSObject <YumiMediationCoreAdapter>
+
+@property (nonatomic, weak) id<YumiMediationCoreAdapterDelegate> delegate;
+@property (nonatomic) YumiMediationCoreProvider *provider;
+
+@end
