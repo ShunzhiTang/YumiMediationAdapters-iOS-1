f8ce1b36a715ef68f76da41d7f78b7e11bce91f8
JiaDingYi <wzy2010416033@163.com>
2019-05-29 10:04:54 +0800

    revert 5caea2f

---
+++ YumiMediationAdapters/ZplayAds/YumiMediationVideoAdapterPlayableAds.h
@@ -0,0 +1,17 @@
+//
+//  YumiMediationVideoAdapterPlayableAds.h
+//  Pods
+//
+//  Created by d on 24/6/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
+
+@interface YumiMediationVideoAdapterPlayableAds : NSObject <YumiMediationCoreAdapter>
+
+@property (nonatomic, weak) id<YumiMediationCoreAdapterDelegate> delegate;
+@property (nonatomic) YumiMediationCoreProvider *provider;
+
+@end
