235945b6d6b147164c02b22a81a6f7022eeae4c7
ShunZhi Tang <tangshunzhi@zplay.com>
2019-05-29 14:48:27 +0800

    Generator BytedanceAds splash adapter

---
+++ YumiMediationAdapters/BytedanceAds/YumiMediationSplashAdapterBytedanceAds.h
@@ -0,0 +1,16 @@
+//
+//  YumiMediationSplashAdapterBytedanceAds.h
+//  YumiMediationAdapters
+//
+//  Created by Michael Tang on 2019/5/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YumiMediationSplashAdapterBytedanceAds : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
