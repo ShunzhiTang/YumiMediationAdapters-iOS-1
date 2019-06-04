307b51ec46ffbf1608a75dbd7f9394beaf16a260
ShunZhi Tang <tangshunzhi@zplay.com>
2019-06-04 20:06:07 +0800

    Add YumiAppOpenViewController

---
+++ YumiMediationAdapters/AdMob/splash/YumiAppOpenViewController.h
@@ -0,0 +1,24 @@
+//
+//  YumiAppOpenViewController.h
+//  YumiMediationAdapters
+//
+//  Created by Michael Tang on 2019/6/4.
+//
+
+#import <UIKit/UIKit.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YumiAppOpenViewController : UIViewController
+
+@property(strong, nonatomic) GADAppOpenAd* appOpenAd;
+@property(strong, nonatomic) IBOutlet GADAppOpenAdView* appOpenAdView;
+@property(nonatomic, copy) void (^onViewControllerClosed)(void);
+
+@property (unsafe_unretained, nonatomic) IBOutlet UIView *bottomView;
+@property (unsafe_unretained, nonatomic) IBOutlet NSLayoutConstraint *adViewHeightConstraint;
+
+@end
+
+NS_ASSUME_NONNULL_END
