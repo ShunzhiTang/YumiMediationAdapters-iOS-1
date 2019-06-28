d7e98d873db18d67e8929695cc01142aec0636d5
ShunZhi Tang <tangshunzhi@zplay.com>
2019-06-28 15:23:46 +0800

    Generator video adapter

---
+++ YumiMediationAdapters/TapjoySDK/YumiMediationVideoAdapterTapjoySDK.m
@@ -0,0 +1,57 @@
+//
+//  YumiMediationVideoAdapterTapjoySDK.m
+//  Pods
+//
+//  Created by generator on 28/06/2019.
+//
+//
+
+#import "YumiMediationVideoAdapterTapjoySDK.h"
+#import <Tapjoy/Tapjoy.h>
+
+@interface YumiMediationVideoAdapterTapjoySDK ()
+
+@property (nonatomic, assign) YumiMediationAdType adType;
+
+@end
+
+@implementation YumiMediationVideoAdapterTapjoySDK
+
++ (void)load {
+    [[YumiMediationAdapterRegistry registry] registerCoreAdapter:self
+                                                   forProviderID:kYumiMediationAdapterIDTapjoy
+                                                     requestType:YumiMediationSDKAdRequest
+                                                          adType:YumiMediationAdTypeVideo];
+}
+
+#pragma mark - YumiMediationVideoAdapter
+- (id<YumiMediationCoreAdapter>)initWithProvider:(YumiMediationCoreProvider *)provider
+                                        delegate:(id<YumiMediationCoreAdapterDelegate>)delegate
+                                          adType:(YumiMediationAdType)adType {
+    self = [super init];
+
+    self.delegate = delegate;
+    self.provider = provider;
+    self.adType = adType;
+
+    // TODO: set code
+
+    return self;
+}
+
+- (void)requestAd {
+    // TODO: request ad
+}
+
+- (BOOL)isReady {
+    // TODO: check if ready
+    return YES;
+}
+
+- (void)presentFromRootViewController:(UIViewController *)rootViewController {
+    // TODO: present video ad
+}
+
+// TODO: implement third party sdk delegate and delegate to mediation sdk
+
+@end
