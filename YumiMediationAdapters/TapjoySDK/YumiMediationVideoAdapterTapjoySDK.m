403133493307cbf3579dc1dccbce07191868198f
ShunZhi Tang <tangshunzhi@zplay.com>
2019-07-01 12:18:42 +0800

    Code format

--- YumiMediationAdapters/TapjoySDK/YumiMediationVideoAdapterTapjoySDK.m
+++ YumiMediationAdapters/TapjoySDK/YumiMediationVideoAdapterTapjoySDK.m
@@ -10,7 +10,7 @@
 #import <Tapjoy/Tapjoy.h>
 #import <YumiMediationSDK/YumiMediationGDPRManager.h>
 
-@interface YumiMediationVideoAdapterTapjoySDK ()<TJPlacementDelegate,TJPlacementVideoDelegate>
+@interface YumiMediationVideoAdapterTapjoySDK () <TJPlacementDelegate, TJPlacementVideoDelegate>
 
 @property (nonatomic, assign) YumiMediationAdType adType;
 @property (strong, nonatomic) TJPlacement *videoPlacement;
@@ -51,10 +51,10 @@
                                              selector:@selector(tjcConnectFail:)
                                                  name:TJC_CONNECT_FAILED
                                                object:nil];
-    
+
     // set gdpr
     YumiMediationConsentStatus gdprStatus = [YumiMediationGDPRManager sharedGDPRManager].getConsentStatus;
-    
+
     if (gdprStatus == YumiMediationConsentStatusPersonalized) {
         [Tapjoy subjectToGDPR:YES];   // 用户遵守GDPR规则
         [Tapjoy setUserConsent:@"1"]; // 用户同意
@@ -63,16 +63,16 @@
         [Tapjoy subjectToGDPR:YES];
         [Tapjoy setUserConsent:@"0"];
     }
-    
-    [Tapjoy connect:self.provider.data.key1 options:@{ TJC_OPTION_ENABLE_LOGGING : @(YES)}];
-    
+
+    [Tapjoy connect:self.provider.data.key1 options:@{ TJC_OPTION_ENABLE_LOGGING : @(YES) }];
+
     return self;
 }
 
 - (void)requestAd {
     // update gdpr
     YumiMediationConsentStatus gdprStatus = [YumiMediationGDPRManager sharedGDPRManager].getConsentStatus;
-    
+
     if (gdprStatus == YumiMediationConsentStatusPersonalized) {
         [Tapjoy subjectToGDPR:YES];
         [Tapjoy setUserConsent:@"1"];
@@ -81,7 +81,7 @@
         [Tapjoy subjectToGDPR:YES];
         [Tapjoy setUserConsent:@"0"];
     }
-    
+
     self.isRewarded = NO;
     self.videoPlacement = [TJPlacement placementWithName:self.provider.data.key2 delegate:self];
     // Set video delegate TJPlacementVideoDelegate
@@ -97,43 +97,36 @@
     [self.videoPlacement showContentWithViewController:rootViewController];
 }
 
-#pragma mark: privare method
--(void)tjcConnectSuccess:(NSNotification*)notifyObj
-{
+#pragma mark : privare method
+- (void)tjcConnectSuccess:(NSNotification *)notifyObj {
     // Remove observer after it's notified once
-    [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                    name:TJC_CONNECT_SUCCESS
-                                                  object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:TJC_CONNECT_SUCCESS object:nil];
 }
 
-- (void)tjcConnectFail:(NSNotification*)notifyObj
-{
+- (void)tjcConnectFail:(NSNotification *)notifyObj {
     // Remove observer after it's notified once
-    [[NSNotificationCenter defaultCenter] removeObserver:self
-                                                    name:TJC_CONNECT_FAILED
-                                                  object:nil];
-    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:TJC_CONNECT_FAILED object:nil];
+
     [self.delegate coreAdapter:self coreAd:nil didFailToLoad:@"Tapjoy connect fail" adType:self.adType];
 }
 
-#pragma mark: TJPlacementDelegate
-- (void)requestDidSucceed:(TJPlacement*)placement {
-    
+#pragma mark : TJPlacementDelegate
+- (void)requestDidSucceed:(TJPlacement *)placement {
 }
 
-- (void)requestDidFail:(TJPlacement*)placement error:(NSError*)error {
-     [self.delegate coreAdapter:self coreAd:nil didFailToLoad:error.localizedDescription adType:self.adType];
+- (void)requestDidFail:(TJPlacement *)placement error:(NSError *)error {
+    [self.delegate coreAdapter:self coreAd:nil didFailToLoad:error.localizedDescription adType:self.adType];
 }
 
-- (void)contentIsReady:(TJPlacement*)placement {
+- (void)contentIsReady:(TJPlacement *)placement {
     [self.delegate coreAdapter:self didReceivedCoreAd:self.videoPlacement adType:self.adType];
 }
 
-- (void)contentDidAppear:(TJPlacement*)placement {
+- (void)contentDidAppear:(TJPlacement *)placement {
     [self.delegate coreAdapter:self didOpenCoreAd:self.videoPlacement adType:self.adType];
 }
 
-- (void)contentDidDisappear:(TJPlacement*)placement {
+- (void)contentDidDisappear:(TJPlacement *)placement {
     if (self.isRewarded) {
         [self.delegate coreAdapter:self coreAd:self.videoPlacement didReward:YES adType:self.adType];
     }
@@ -142,21 +135,20 @@
              isCompletePlaying:self.isRewarded
                         adType:self.adType];
     self.isRewarded = NO;
-    
 }
-- (void)didClick:(TJPlacement*)placement {
+- (void)didClick:(TJPlacement *)placement {
     [self.delegate coreAdapter:self didClickCoreAd:self.videoPlacement adType:self.adType];
 }
-#pragma mark: TJPlacementVideoDelegate
-- (void)videoDidStart:(TJPlacement*)placement {
+#pragma mark : TJPlacementVideoDelegate
+- (void)videoDidStart:(TJPlacement *)placement {
     [self.delegate coreAdapter:self didStartPlayingAd:self.videoPlacement adType:self.adType];
 }
 
-- (void)videoDidComplete:(TJPlacement*)placement {
+- (void)videoDidComplete:(TJPlacement *)placement {
     self.isRewarded = YES;
 }
 
-- (void)videoDidFail:(TJPlacement*)placement error:(NSString*)errorMsg {
+- (void)videoDidFail:(TJPlacement *)placement error:(NSString *)errorMsg {
     [self.delegate coreAdapter:self failedToShowAd:self.videoPlacement errorString:errorMsg adType:self.adType];
 }
 
