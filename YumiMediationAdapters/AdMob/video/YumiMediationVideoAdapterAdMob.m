074d1f727c1547fe8ebee39597212e172ffc0eeb
ShunZhi Tang <tangshunzhi@zplay.com>
2019-07-08 18:56:34 +0800

    Code format

--- YumiMediationAdapters/AdMob/video/YumiMediationVideoAdapterAdMob.m
+++ YumiMediationAdapters/AdMob/video/YumiMediationVideoAdapterAdMob.m
@@ -13,7 +13,7 @@
 @interface YumiMediationVideoAdapterAdMob () <GADRewardedAdDelegate>
 @property (nonatomic, assign) BOOL isReward;
 @property (nonatomic, assign) YumiMediationAdType adType;
-@property(nonatomic, strong) GADRewardedAd *rewardedAd;
+@property (nonatomic, strong) GADRewardedAd *rewardedAd;
 
 @end
 
@@ -37,12 +37,12 @@
     self.delegate = delegate;
     self.provider = provider;
     self.adType = adType;
-    
+
     NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
     if ([standardUserDefaults objectForKey:YumiMediationAdmobAdapterUUID]) {
         return self;
     }
-    
+
     [[GADMobileAds sharedInstance] startWithCompletionHandler:^(GADInitializationStatus *_Nonnull status) {
         [standardUserDefaults setObject:@"Admob_is_starting" forKey:YumiMediationAdmobAdapterUUID];
         [standardUserDefaults synchronize];
@@ -52,39 +52,40 @@
 }
 
 - (void)requestAd {
-    
+
     self.isReward = NO;
     // set GDPR
     YumiMediationConsentStatus gdprStatus = [YumiMediationGDPRManager sharedGDPRManager].getConsentStatus;
-    
+
     GADExtras *extras = [[GADExtras alloc] init];
     if (gdprStatus == YumiMediationConsentStatusPersonalized) {
-        extras.additionalParameters = @{@"npa": @"0"};
+        extras.additionalParameters = @{ @"npa" : @"0" };
     }
     if (gdprStatus == YumiMediationConsentStatusNonPersonalized) {
-        extras.additionalParameters = @{@"npa": @"1"};
+        extras.additionalParameters = @{ @"npa" : @"1" };
     }
-    
+
     GADRequest *request = [GADRequest request];
     [request registerAdNetworkExtras:extras];
-    
+
     __weak typeof(self) weakSelf = self;
-    
+
     self.rewardedAd = [[GADRewardedAd alloc] initWithAdUnitID:self.provider.data.key1];
-    
-    [self.rewardedAd loadRequest:request completionHandler:^(GADRequestError * _Nullable error) {
-        if (error) {
-            [weakSelf.delegate coreAdapter:weakSelf
-                                coreAd:weakSelf.rewardedAd
-                         didFailToLoad:[error localizedDescription]
-                                adType:weakSelf.adType];
-            return ;
-        }
-        //  Ad successfully loaded.
-        [weakSelf.delegate coreAdapter:weakSelf didReceivedCoreAd:weakSelf.rewardedAd adType:weakSelf.adType];
-        
-    }];
-    
+
+    [self.rewardedAd
+              loadRequest:request
+        completionHandler:^(GADRequestError *_Nullable error) {
+            if (error) {
+                [weakSelf.delegate coreAdapter:weakSelf
+                                        coreAd:weakSelf.rewardedAd
+                                 didFailToLoad:[error localizedDescription]
+                                        adType:weakSelf.adType];
+                return;
+            }
+            //  Ad successfully loaded.
+            [weakSelf.delegate coreAdapter:weakSelf didReceivedCoreAd:weakSelf.rewardedAd adType:weakSelf.adType];
+
+        }];
 }
 
 - (BOOL)isReady {
@@ -97,15 +98,16 @@
 
 #pragma mark - GADRewardedAdDelegate
 /// Tells the delegate that the user earned a reward.
-- (void)rewardedAd:(nonnull GADRewardedAd *)rewardedAd
- userDidEarnReward:(nonnull GADAdReward *)reward {
+- (void)rewardedAd:(nonnull GADRewardedAd *)rewardedAd userDidEarnReward:(nonnull GADAdReward *)reward {
     self.isReward = YES;
 }
 
 /// Tells the delegate that the rewarded ad failed to present.
-- (void)rewardedAd:(nonnull GADRewardedAd *)rewardedAd
-didFailToPresentWithError:(nonnull NSError *)error {
-    [self.delegate coreAdapter:self failedToShowAd:self.rewardedAd errorString:[error localizedDescription] adType:self.adType];
+- (void)rewardedAd:(nonnull GADRewardedAd *)rewardedAd didFailToPresentWithError:(nonnull NSError *)error {
+    [self.delegate coreAdapter:self
+                failedToShowAd:self.rewardedAd
+                   errorString:[error localizedDescription]
+                        adType:self.adType];
 }
 
 /// Tells the delegate that the rewarded ad was presented.
@@ -119,10 +121,7 @@ didFailToPresentWithError:(nonnull NSError *)error {
     if (self.isReward) {
         [self.delegate coreAdapter:self coreAd:self.rewardedAd didReward:YES adType:self.adType];
     }
-    [self.delegate coreAdapter:self
-                didCloseCoreAd:self.rewardedAd
-             isCompletePlaying:self.isReward
-                        adType:self.adType];
+    [self.delegate coreAdapter:self didCloseCoreAd:self.rewardedAd isCompletePlaying:self.isReward adType:self.adType];
     self.isReward = NO;
 }
 
