95dd9f0803735fe381ca7edd7b68a3ad5e2f245d
ShunZhi Tang <tangshunzhi@zplay.com>
2019-07-16 18:27:52 +0800

    Baidu Interstitial support adSize

--- YumiMediationAdapters/Baidu/YumiMediationInterstitialAdapterBaidu.m
+++ YumiMediationAdapters/Baidu/YumiMediationInterstitialAdapterBaidu.m
@@ -8,12 +8,17 @@
 
 #import "YumiMediationInterstitialAdapterBaidu.h"
 #import <BaiduMobAdSDK/BaiduMobAdInterstitial.h>
+#import <YumiMediationSDK/YumiTool.h>
+#import <YumiMediationSDK/YumiMasonry.h>
 
 @interface YumiMediationInterstitialAdapterBaidu () <BaiduMobAdInterstitialDelegate>
 
 @property (nonatomic) BaiduMobAdInterstitial *interstitial;
 @property (nonatomic, assign) YumiMediationAdType adType;
 @property (nonatomic, assign) BOOL interstitialIsReady;
+
+@property (nonatomic) UIView *customInterView;
+@property (nonatomic, assign) CGSize adSize;
 
 @end
 
@@ -45,13 +50,28 @@
 }
 
 - (void)requestAd {
+    
     __weak typeof(self) weakSelf = self;
     dispatch_async(dispatch_get_main_queue(), ^{
         weakSelf.interstitial = [[BaiduMobAdInterstitial alloc] init];
         weakSelf.interstitial.delegate = weakSelf;
         weakSelf.interstitial.AdUnitTag = weakSelf.provider.data.key2;
-        weakSelf.interstitial.interstitialType = BaiduMobAdViewTypeInterstitialOther;
-        [weakSelf.interstitial load];
+        weakSelf.interstitial.interstitialType = BaiduMobAdViewTypeInterstitialBeforeVideo;
+        
+        //aspectRatio = width : height
+        float aspectRatio = [self.provider.data.extra[YumiProviderExtraBaidu] floatValue];
+        
+        if (aspectRatio == 0) {
+            [weakSelf.interstitial load];
+            return ;
+        }
+       
+        float width = MIN(kSCREEN_WIDTH, kSCREEN_HEIGHT);
+        float height = width / aspectRatio;
+        
+        self.adSize = CGSizeMake(width, height);
+        
+        [weakSelf.interstitial loadUsingSize:CGRectMake(0, 0, width, height)];
     });
 }
 
@@ -60,7 +80,26 @@
 }
 
 - (void)presentFromRootViewController:(UIViewController *)rootViewController {
-    [self.interstitial presentFromRootViewController:rootViewController];
+    
+    float aspectRatio = [self.provider.data.extra[YumiProviderExtraBaidu] floatValue];
+    
+    if (aspectRatio == 0) {
+        [self.interstitial presentFromRootViewController:rootViewController];
+        return ;
+    }
+    
+    self.customInterView = [[UIView alloc] init];;
+    [rootViewController.view addSubview:self.customInterView];
+    
+    [self.customInterView mas_makeConstraints:^(YumiMASConstraintMaker *make) {
+        make.center.equalTo(rootViewController.view);
+        make.height.mas_equalTo(self.adSize.height);
+        make.width.mas_equalTo(self.adSize.width);
+    }];
+    [rootViewController.view layoutIfNeeded];
+    
+    [self.interstitial presentFromView:self.customInterView];
+    
 }
 
 #pragma mark - BaiduMobAdInterstitialDelegate
@@ -91,6 +130,7 @@
                 failedToShowAd:interstitial
                    errorString:@"Baidu ad failed to show"
                         adType:self.adType];
+    [self clearInterstitial];
 }
 
 - (void)interstitialDidAdClicked:(BaiduMobAdInterstitial *)interstitial {
@@ -104,6 +144,12 @@
 }
 
 - (void)clearInterstitial {
+    
+    if (self.customInterView) {
+        [self.customInterView removeFromSuperview];
+        self.customInterView = nil;
+    }
+    
     if (self.interstitial) {
         self.interstitial.delegate = nil;
         self.interstitial = nil;
