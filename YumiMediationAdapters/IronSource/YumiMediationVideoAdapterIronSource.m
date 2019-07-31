afb2daa22e4337c565526d492a831fb1c605a576
ShunZhi Tang <tangshunzhi@zplay.com>
2019-07-31 16:57:33 +0800

    Fixed ironsource isReward

--- YumiMediationAdapters/IronSource/YumiMediationVideoAdapterIronSource.m
+++ YumiMediationAdapters/IronSource/YumiMediationVideoAdapterIronSource.m
@@ -117,9 +117,10 @@
 - (void)rewardedVideoDidClose:(NSString *)instanceId {
     if (self.isReward) { // ironsource 确保无中途关闭并且奖励回调始终在关闭之前
         [self.delegate coreAdapter:self coreAd:nil didReward:YES adType:self.adType];
-        self.isReward = NO;
     }
     [self.delegate coreAdapter:self didCloseCoreAd:nil isCompletePlaying:self.isReward adType:self.adType];
+    
+    self.isReward = NO;
 }
 
 // Invoked when the end user clicked on the RewardedVideo ad
