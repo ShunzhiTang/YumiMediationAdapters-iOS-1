d3361dfa6316df41b647e6fa18b2a51b579c77ff
ShunZhi Tang <tangshunzhi@zplay.com>
2019-08-12 17:34:41 +0800

    Implement chartboost  banner adapter

--- YumiMediationAdapters/Chartboost/YumiMediationBannerAdapterChartboost.m
+++ YumiMediationAdapters/Chartboost/YumiMediationBannerAdapterChartboost.m
@@ -7,12 +7,20 @@
 //
 
 #import "YumiMediationBannerAdapterChartboost.h"
+#import <Chartboost/CHBBanner.h>
+#import <Chartboost/Chartboost.h>
 #import <YumiMediationSDK/YumiMediationAdapterRegistry.h>
+#import <YumiMediationSDK/YumiMediationGDPRManager.h>
 
-@interface YumiMediationBannerAdapterChartboost () <YumiMediationBannerAdapter>
+@interface YumiMediationBannerAdapterChartboost () <YumiMediationBannerAdapter, ChartboostDelegate, CHBBannerDelegate>
 
 @property (nonatomic, weak) id<YumiMediationBannerAdapterDelegate> delegate;
 @property (nonatomic) YumiMediationBannerProvider *provider;
+
+@property (nonatomic, assign) YumiMediationAdViewBannerSize bannerSize;
+@property (nonatomic, assign) BOOL isSmartBanner;
+
+@property (nonatomic, strong) CHBBanner *banner;
 
 @end
 
@@ -32,15 +40,101 @@
     self.delegate = delegate;
     self.provider = provider;
 
-    // TODO: setup code
+    // set GDPR
+    YumiMediationConsentStatus gdprStatus = [YumiMediationGDPRManager sharedGDPRManager].getConsentStatus;
+
+    if (gdprStatus == YumiMediationConsentStatusPersonalized) {
+        [Chartboost setPIDataUseConsent:YesBehavioral];
+    }
+    if (gdprStatus == YumiMediationConsentStatusNonPersonalized) {
+        [Chartboost setPIDataUseConsent:NoBehavioral];
+    }
+
+    [Chartboost startWithAppId:self.provider.data.key1 appSignature:self.provider.data.key2 delegate:self];
 
     return self;
 }
 
-- (void)requestAdWithIsPortrait:(BOOL)isPortrait isiPad:(BOOL)isiPad {
-    // TODO: request ad
+- (void)setBannerSizeWith:(YumiMediationAdViewBannerSize)adSize smartBanner:(BOOL)isSmart {
+    self.bannerSize = adSize;
+    self.isSmartBanner = isSmart;
 }
 
-// TODO: implement third party sdk delegate and delegate to mediation sdk
+- (void)requestAdWithIsPortrait:(BOOL)isPortrait isiPad:(BOOL)isiPad {
+    if (self.bannerSize == kYumiMediationAdViewSmartBannerPortrait ||
+        self.bannerSize == kYumiMediationAdViewSmartBannerLandscape) {
+        [self.delegate adapter:self
+            didFailToReceiveAd:@"Chartboost not support kYumiMediationAdViewSmartBannerPortrait or "
+                               @"kYumiMediationAdViewSmartBannerLandscape"];
+        return;
+    }
+
+    __weak typeof(self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (weakSelf.bannerSize == kYumiMediationAdViewBanner300x250) {
+            /// CHBBannerSizeMedium = 300 x 250
+            weakSelf.banner =
+                [[CHBBanner alloc] initWithSize:CHBBannerSizeMedium location:CBLocationDefault delegate:weakSelf];
+        } else {
+            if (isiPad) {
+                /// CHBBannerSizeLeaderboard = 728 x 90
+                weakSelf.banner = [[CHBBanner alloc] initWithSize:CHBBannerSizeLeaderboard
+                                                         location:CBLocationDefault
+                                                         delegate:weakSelf];
+            }
+            // iphone
+            if (!isiPad) {
+                /// CHBBannerSizeStandard = 320 x 50
+                weakSelf.banner = [[CHBBanner alloc] initWithSize:CHBBannerSizeStandard
+                                                         location:CBLocationHomeScreen
+                                                         delegate:weakSelf];
+            }
+        }
+        // Manually handling banner refresh
+        if (weakSelf.provider.data.autoRefreshInterval == 0) {
+            weakSelf.banner.automaticallyRefreshesContent = NO;
+            [weakSelf.banner cache];
+        } else {
+            // Auto-refreshing
+            weakSelf.banner.automaticallyRefreshesContent = YES;
+            [weakSelf.banner showFromViewController:[weakSelf.delegate rootViewControllerForPresentingModalView]];
+        }
+
+    });
+}
+
+#pragma mark - ChartboostDelegate
+///  status The result of the initialization. YES if successful. NO if failed.
+- (void)didInitialize:(BOOL)status {
+    if (!status) {
+        [self.delegate adapter:self didFailToReceiveAd:@"Chartboost initialize fail.."];
+    }
+}
+
+#pragma mark : CHBBannerDelegate
+- (void)didCacheAd:(CHBCacheEvent *)event error:(nullable CHBCacheError *)error {
+    if (error || !self.banner.isCached) {
+        [self.delegate adapter:self
+            didFailToReceiveAd:[NSString stringWithFormat:@"Chartboost load fail. error code is %ld", error.code]];
+        return;
+    }
+    // Show banner after it has been cached if Manually handling refresh
+    if (!self.banner.automaticallyRefreshesContent) {
+        [self.banner showFromViewController:[self.delegate rootViewControllerForPresentingModalView]];
+    }
+
+    [self.delegate adapter:self didReceiveAd:self.banner];
+}
+
+- (void)didShowAd:(CHBShowEvent *)event error:(nullable CHBShowError *)error {
+    if (error) {
+        [self.delegate adapter:self
+            didFailToReceiveAd:[NSString stringWithFormat:@"Chartboost show fail. error code is %ld", error.code]];
+    }
+}
+
+- (void)didClickAd:(CHBClickEvent *)event error:(nullable CHBClickError *)error {
+    [self.delegate adapter:self didClick:self.banner];
+}
 
 @end
