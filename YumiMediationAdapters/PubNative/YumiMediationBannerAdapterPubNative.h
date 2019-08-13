252fdc28d96aba619f81db80ff8f1334a02f296e
ShunZhi Tang <tangshunzhi@zplay.com>
2019-08-13 12:02:38 +0800

    Generator pubNative banner adapter

---
+++ YumiMediationAdapters/PubNative/YumiMediationBannerAdapterPubNative.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationBannerAdapterPubNative.h
+//  Pods
+//
+//  Created by generator on 13/08/2019.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationBannerAdapterPubNative : NSObject
+
+@end
