f965293a46d4415c03c357169a605de352b1f867
ShunZhi Tang <tangshunzhi@zplay.com>
2019-09-16 14:17:18 +0800

    Adapters modify presentation style yo full screen

--- YumiMediationAdapters/Facebook/interstitial/YumiFacebookAdapterInterstitialVc.m
+++ YumiMediationAdapters/Facebook/interstitial/YumiFacebookAdapterInterstitialVc.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    //Fixed iOS 13 modalPresentationStyle
+    self.modalPresentationStyle = UIModalPresentationFullScreen;
 }
 
 - (void)didReceiveMemoryWarning {
