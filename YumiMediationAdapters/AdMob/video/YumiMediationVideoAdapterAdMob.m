f9d4e56c9f1367359cb2905bf281297317aa6c18
JiaDingYi <wzy2010416033@163.com>
2019-10-23 15:13:44 +0800

    finetune admob video adapter

--- YumiMediationAdapters/AdMob/video/YumiMediationVideoAdapterAdMob.m
+++ YumiMediationAdapters/AdMob/video/YumiMediationVideoAdapterAdMob.m
@@ -18,6 +18,9 @@
 @end
 
 @implementation YumiMediationVideoAdapterAdMob
+- (NSString *)networkVersion {
+    return @"7.50.0";
+}
 
 + (void)load {
     [[YumiMediationAdapterRegistry registry] registerCoreAdapter:self
@@ -37,22 +40,10 @@
     self.delegate = delegate;
     self.provider = provider;
     self.adType = adType;
-
-    NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
-    if ([standardUserDefaults objectForKey:YumiMediationAdmobAdapterUUID]) {
-        return self;
-    }
-
-    [[GADMobileAds sharedInstance] startWithCompletionHandler:^(GADInitializationStatus *_Nonnull status) {
-        [standardUserDefaults setObject:@"Admob_is_starting" forKey:YumiMediationAdmobAdapterUUID];
-        [standardUserDefaults synchronize];
-    }];
+    self.isReward = NO;
+    self.rewardedAd = nil;
 
     return self;
-}
-
-- (NSString *)networkVersion {
-    return @"7.50.0";
 }
 
 - (void)updateProviderData:(YumiMediationCoreProvider *)provider {
@@ -60,11 +51,23 @@
 }
 
 - (void)requestAd {
+    NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    if ([standardUserDefaults objectForKey:YumiMediationAdmobAdapterUUID]) {
+        [self requestAdmobVideo];
+        return;
+    }
+    __weak __typeof(self)weakSelf = self;
+    [[GADMobileAds sharedInstance] startWithCompletionHandler:^(GADInitializationStatus *_Nonnull status) {
+        [standardUserDefaults setObject:@"Admob_is_starting" forKey:YumiMediationAdmobAdapterUUID];
+        [standardUserDefaults synchronize];
+        [weakSelf requestAdmobVideo];
+    }];
 
-    self.isReward = NO;
+}
+
+- (void)requestAdmobVideo {
     // set GDPR
     YumiMediationConsentStatus gdprStatus = [YumiMediationGDPRManager sharedGDPRManager].getConsentStatus;
-
     GADExtras *extras = [[GADExtras alloc] init];
     if (gdprStatus == YumiMediationConsentStatusPersonalized) {
         extras.additionalParameters = @{@"npa" : @"0"};
@@ -72,14 +75,11 @@
     if (gdprStatus == YumiMediationConsentStatusNonPersonalized) {
         extras.additionalParameters = @{@"npa" : @"1"};
     }
-
     GADRequest *request = [GADRequest request];
     [request registerAdNetworkExtras:extras];
-
+    
     __weak typeof(self) weakSelf = self;
-
     self.rewardedAd = [[GADRewardedAd alloc] initWithAdUnitID:self.provider.data.key1];
-
     [self.rewardedAd
               loadRequest:request
         completionHandler:^(GADRequestError *_Nullable error) {
@@ -131,6 +131,7 @@
     }
     [self.delegate coreAdapter:self didCloseCoreAd:self.rewardedAd isCompletePlaying:self.isReward adType:self.adType];
     self.isReward = NO;
+    self.rewardedAd = nil;
 }
 
 @end
