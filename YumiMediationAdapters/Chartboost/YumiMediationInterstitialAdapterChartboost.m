1b9ccaef4f65f8e8f0ab243c4af1a04fbf9a6dc3
JiaDingYi <wzy2010416033@163.com>
2019-10-28 17:40:37 +0800

    finetune chartboost interstitial adapter

--- YumiMediationAdapters/Chartboost/YumiMediationInterstitialAdapterChartboost.m
+++ YumiMediationAdapters/Chartboost/YumiMediationInterstitialAdapterChartboost.m
@@ -9,14 +9,17 @@
 #import "YumiMediationInterstitialAdapterChartboost.h"
 #import <Chartboost/Chartboost.h>
 #import <YumiMediationSDK/YumiMediationGDPRManager.h>
+#import <YumiMediationSDK/YumiLogger.h>
 
 @interface YumiMediationInterstitialAdapterChartboost () <ChartboostDelegate>
-
 @property (nonatomic, assign) YumiMediationAdType adType;
 
 @end
 
 @implementation YumiMediationInterstitialAdapterChartboost
+- (NSString *)networkVersion {
+    return @"8.0.1";
+}
 
 + (void)load {
     [[YumiMediationAdapterRegistry registry] registerCoreAdapter:self
@@ -44,14 +47,11 @@
     if (gdprStatus == YumiMediationConsentStatusNonPersonalized) {
         [Chartboost setPIDataUseConsent:NoBehavioral];
     }
-
+    
+    [[YumiLogger stdLogger] debug:@"---chartboost start init"];
     [Chartboost startWithAppId:self.provider.data.key1 appSignature:self.provider.data.key2 delegate:self];
     [Chartboost setAutoCacheAds:NO];
     return self;
-}
-
-- (NSString *)networkVersion {
-    return @"8.0.1";
 }
 
 - (void)updateProviderData:(YumiMediationCoreProvider *)provider {
@@ -59,7 +59,6 @@
 }
 
 - (void)requestAd {
-
     // update GDPR
     YumiMediationConsentStatus gdprStatus = [YumiMediationGDPRManager sharedGDPRManager].getConsentStatus;
 
@@ -69,25 +68,29 @@
     if (gdprStatus == YumiMediationConsentStatusNonPersonalized) {
         [Chartboost setPIDataUseConsent:NoBehavioral];
     }
-
+    [[YumiLogger stdLogger] debug:@"---chartboost start request"];
     [Chartboost cacheInterstitial:CBLocationDefault];
 }
 
 - (BOOL)isReady {
+    NSString *msg = [NSString stringWithFormat:@"---chartboost check ready status.%d",[Chartboost hasInterstitial:CBLocationDefault]];
+    [[YumiLogger stdLogger] debug:msg];
     return [Chartboost hasInterstitial:CBLocationDefault];
 }
 
 - (void)presentFromRootViewController:(UIViewController *)rootViewController {
+    [[YumiLogger stdLogger] debug:@"---chartboost present"];
     [Chartboost showInterstitial:CBLocationDefault];
 }
 
 #pragma mark - ChartboostDelegate
-
 - (void)didCacheInterstitial:(CBLocation)location {
+    [[YumiLogger stdLogger] debug:@"---chartboost did load"];
     [self.delegate coreAdapter:self didReceivedCoreAd:nil adType:self.adType];
 }
 
 - (void)didFailToLoadInterstitial:(CBLocation)location withError:(CBLoadError)error {
+    [[YumiLogger stdLogger] debug:@"---chartboost did fail to load"];
     [self.delegate coreAdapter:self
                         coreAd:nil
                  didFailToLoad:[NSString stringWithFormat:@"Chartboost error code: %@", @(error)]
@@ -109,6 +112,7 @@
 }
 
 - (void)didDismissInterstitial:(CBLocation)location {
+    [[YumiLogger stdLogger] debug:@"---chartboost did closed"];
     [self.delegate coreAdapter:self didCloseCoreAd:nil isCompletePlaying:NO adType:self.adType];
 }
 
