52b6a82390d403a3b3b10a0ccb76861d55392b50
JiaDingYi <wzy2010416033@163.com>
2019-10-31 11:27:40 +0800

    finetune unity adapter debug log

--- YumiMediationAdapters/Unity/YumiMediationUnityInstance.m
+++ YumiMediationAdapters/Unity/YumiMediationUnityInstance.m
@@ -99,10 +99,12 @@ static NSString *separatedString = @"|||";
     id<YumiMediationCoreAdapter> adapter = [self adapterObject:placementId];
     if (adType == YumiMediationAdTypeInterstitial) {
         [((YumiMediationInterstitialAdapterUnity *)adapter).delegate coreAdapter:adapter didReceivedCoreAd:nil adType:YumiMediationAdTypeInterstitial];
+        [[YumiLogger stdLogger] debug:@"---Unity video did load"];
         return;
     }
     if (adType == YumiMediationAdTypeVideo) {
         [((YumiMediationVideoAdapterUnity *)adapter).delegate coreAdapter:adapter didReceivedCoreAd:nil adType:YumiMediationAdTypeVideo];
+        [[YumiLogger stdLogger] debug:@"---Unity video did load"];
     }
 }
 
@@ -208,7 +210,7 @@ static NSString *separatedString = @"|||";
                              oldState:(UnityAdsPlacementState)oldState
                              newState:(UnityAdsPlacementState)newState {
     
-    NSString *stateString = [NSString stringWithFormat:@"placementId == %@ ,oldState -- %ld ,newState = %ld",placementId,oldState,newState];
+    NSString *stateString = [NSString stringWithFormat:@"---Unity placementId == %@ ,oldState -- %ld ,newState = %ld",placementId,oldState,newState];
     [[YumiLogger stdLogger] debug:stateString];
     
     NSUInteger adType = [self adapterAdType:placementId];
