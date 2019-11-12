25cb0f56f23a5f4b46358100e2193cfec0c70a52
ShunZhi Tang <tangshunzhi@zplay.com>
2019-11-12 15:58:16 +0800

    Add ZplayAds banner adapter

---
+++ YumiMediationAdapters/ZplayAds/YumiMediationBannerAdapterPlayableAds.h
@@ -0,0 +1,13 @@
+//
+//  YumiMediationBannerAdapterPlayableAds.h
+//  Pods
+//
+//  Created by generator on 12/11/2019.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YumiMediationBannerAdapterPlayableAds : NSObject
+
+@end
